fdb9844364e1a96ee5bd9e14c07440af1c784c50
Brad Kimmel <intrepidca@gmail.com>
2007-05-21 16:46:24 +0000

    Added framework for event handling (IEvent, Event, IEventHandler).

---
+++ src/org/jmist/framework/event/Event.java
@@ -0,0 +1,47 @@
+/**
+ *
+ */
+package org.jmist.framework.event;
+
+import java.util.Set;
+import java.util.LinkedHashSet;
+
+/**
+ * Represents an event that can occur and maintains a list of subscribers
+ * to be notified when the event is raised.
+ * @author bkimmel
+ */
+public class Event<T> implements IEvent<T> {
+
+	/* (non-Javadoc)
+	 * @see org.jmist.framework.event.IEvent#subscribe(org.jmist.framework.event.IEventHandler)
+	 */
+	public void subscribe(IEventHandler<T> handler) {
+		observers.add(handler);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jmist.framework.event.IEvent#unsubscribe(org.jmist.framework.event.IEventHandler)
+	 */
+	public void unsubscribe(IEventHandler<T> handler) {
+		observers.remove(handler);
+	}
+
+	/**
+	 * Notifies observers that this event has occurred.
+	 * @param sender The object that is the subject of this event.
+	 * @param args Additional information pertaining to this event.
+	 */
+	public void raise(Object sender, T args) {
+		for (IEventHandler<T> observer : observers) {
+			observer.notify(sender, args);
+		}
+	}
+
+	/**
+	 * The collection of observers to be notified when the
+	 * event is raised.
+	 */
+	private Set<IEventHandler<T>> observers = new LinkedHashSet<IEventHandler<T>>();
+
+}
