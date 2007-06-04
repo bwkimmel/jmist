59322a803b999cc8c72ff3196399c03444e04df5
Brad Kimmel <intrepidca@gmail.com>
2007-06-04 02:06:23 +0000

    Added Circle.intersects to determine if a ray intersects this circle. Added Circle.intersect to compute the intersection of a ray with this circle.

--- src/org/jmist/toolkit/Circle.java
+++ src/org/jmist/toolkit/Circle.java
@@ -110,6 +110,99 @@ public final class Circle {
 	}
 
 	/**
+	 * Determines whether the specified ray intersects with
+	 * this circle.  Equivalent to {@code !this.intersect(ray).isEmpty()}.
+	 * @param ray The ray to test for an intersection with.
+	 * @return A value indicating whether the ray intersects
+	 * 		this circle.
+	 * @see {@link #intersect(Ray2)}, {@link Interval#isEmpty()}.
+	 */
+	public boolean intersects(Ray2 ray) {
+
+		//
+		// Algorithm from:
+		//
+		// A.S. Glassner, Ed.,
+		// "An Introduction to Ray Tracing",
+		// Morgan Kaufmann Publishers, Inc., San Francisco, CA, 2002
+		// Section 2.2
+		//
+
+		if (isEmpty()) {
+			return false;
+		}
+
+		double		r2 = radius * radius;
+		Vector2		oc = ray.getOrigin().vectorTo(center);
+		double		L2oc = oc.dot(oc);
+
+		if (L2oc < r2) return true;
+
+		double		tca = oc.dot(ray.getDirection());
+
+		if (tca < 0.0) return false;
+
+		return ray.pointAt(tca).squaredDistanceTo(center) < r2;
+
+	}
+
+	/**
+	 * Computes the intersection of this circle with the given
+	 * ray.
+	 * @param ray The ray to compute the intersection of this
+	 * 		circle with.
+	 * @return The interval in which the ray passes through
+	 * 		the circle (i.e., this.contains(ray.pointAt(x)) if and
+	 * 		only if this.intersect(ray).contains(x)).
+	 * @see {@link #contains(Point2)}, {@link Ray2#pointAt(double)},
+	 * 		{@link Interval#contains(double)}.
+	 */
+	public Interval intersect(Ray2 ray) {
+
+		//
+		// Algorithm from:
+		//
+		// A.S. Glassner, Ed.,
+		// "An Introduction to Ray Tracing",
+		// Morgan Kaufmann Publishers, Inc., San Francisco, CA, 2002
+		// Section 2.2
+		//
+
+		// Check for an empty box.
+		if (isEmpty()) {
+			return Interval.EMPTY;
+		}
+
+		// Check if the ray starts from within the box.
+		double		r2 = radius * radius;
+		Vector2		oc = ray.getOrigin().vectorTo(center);
+		double		L2oc = oc.dot(oc);
+		boolean		startInside = (L2oc < r2);
+
+		// distance along ray to point on ray closest to center of sphere (equation (A10)).
+		double		tca = oc.dot(ray.getDirection());
+
+		// if the ray starts outside the sphere and points away from the center of the
+		// sphwere, then the ray does not hit the sphere.
+		if (!startInside && tca < 0.0) {
+			return Interval.EMPTY;
+		}
+
+		// compute half chord distance squared (equation (A13)).
+		double		t2hc = r2 - L2oc + (tca * tca);
+
+		if (t2hc < 0.0) {
+			return Interval.EMPTY;
+		}
+
+		double		thc = Math.sqrt(t2hc);
+
+		// compute interval (equation (A14)).
+		return new Interval(startInside ? 0.0 : (tca - thc), tca + thc);
+
+	}
+
+	/**
 	 * Default constructor.
 	 */
 	private Circle() {
