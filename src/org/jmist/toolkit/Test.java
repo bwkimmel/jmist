a3f4868a3bce18d47ff1ebf7d8d4c3ff98e4ced2
Brad Kimmel <intrepidca@gmail.com>
2007-06-04 05:03:08 +0000

    Updated test class.

--- src/org/jmist/toolkit/Test.java
+++ src/org/jmist/toolkit/Test.java
@@ -1,8 +1,10 @@
 package org.jmist.toolkit;
 
-import java.lang.reflect.InvocationTargetException;
-
 import org.jmist.framework.event.*;
+import org.jmist.util.*;
+import org.jmist.toolkit.Grid3.Cell;
+
+import java.lang.ref.*;
 
 public class Test {
 
@@ -71,20 +73,20 @@ public class Test {
 		}
 
 		public void print() {
-			System.out.println("last event A arg = " + Integer.toString(lastEventAArg));
+			System.out.println("last event A arg = " + lastEventAArg.toString());
 		}
 
 		private TestSubject subject;
-		private int lastEventAArg = 0;
+		private Object lastEventAArg = new Integer(0);
 
-		private IEventHandler<Object> eventBHandler = new IEventHandler<Object>() {
+		private IEventHandler eventBHandler = new IEventHandler() {
 			public void notify(Object sender, Object args) {
 				System.out.println("event B raised");
 			}
 		};
 
-		private IEventHandler<Integer> eventAHandler = new IEventHandler<Integer>() {
-			public void notify(Object sender, Integer arg) {
+		private IEventHandler eventAHandler = new IEventHandler() {
+			public void notify(Object sender, Object arg) {
 				lastEventAArg = arg;
 				System.out.println("event A raised, arg = " + arg.toString());
 			}
@@ -96,7 +98,6 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
@@ -104,6 +105,15 @@ public class Test {
 			}
 			System.out.println();
 		}
+
+		Vector3 I = new Vector3(1.0, 1.0, 1.0).unit();
+		Vector3 R = Optics.reflect(I, Vector3.J);
+
+		Object x = new Integer(5);
+		Reference<Object> ref1 = new WeakReference<Object>(x);
+		Reference<Object> ref2 = new WeakReference<Object>(x);
+
+		System.out.println(ref1.get().equals(ref2.get()));
 
 		TestSubject subject = new TestSubject();
 		TestObserver observer = new TestObserver(subject);
@@ -121,6 +131,69 @@ public class Test {
 		subject.raiseEventB();
 		observer.enableEventB();
 		subject.raiseEventB();
+
+		Grid3 grid = new Grid3(new Box3(0.0, 0.0, 0.0, 1.0, 1.0, 1.0), 4, 4, 4);
+		Ray3 ray = new Ray3(new Point3(-1.0, -1.5, -1.0), new Vector3(1.4, 2.1, 1.3).unit());
+
+		Grid3.IVisitor visitor = new Grid3.IVisitor() {
+
+			/* (non-Javadoc)
+			 * @see org.jmist.toolkit.Grid3.IVisitor#visit(org.jmist.toolkit.Ray3, org.jmist.toolkit.Interval, org.jmist.toolkit.Grid3.Cell)
+			 */
+			public boolean visit(Ray3 ray, Interval I, Cell cell) {
+
+				System.out.println("I=[" + I.getMinimum() + ", " + I.getMaximum() + "]");
+				System.out.println("cell=[" + cell.getX() + " " + cell.getY() + " " + cell.getZ() + "]");
+
+				Point3		p0 = ray.pointAt(I.getMinimum());
+				Point3		p1 = ray.pointAt(I.getMaximum());
+				Vector3		N0 = cell.getBoundingBox().normalAt(p0);
+				Vector3		N1 = cell.getBoundingBox().normalAt(p1);
+
+				System.out.println("p0=(" + p0.x() + ", " + p0.y() + ", " + p0.z() + "); N0=(" + N0.x() + ", " + N0.y() + ", " + N0.z() + ")");
+				System.out.println("p1=(" + p1.x() + ", " + p1.y() + ", " + p1.z() + "); N1=(" + N1.x() + ", " + N1.y() + ", " + N1.z() + ")");
+
+				System.out.println("-----------------------------");
+
+				return true;
+
+			}
+
+		};
+
+		grid.intersect(ray, Interval.POSITIVE, visitor);
+
+		boolean passed = true;
+		java.util.Random rnd = new java.util.Random();
+
+		for (int i = 0; i < 1000; i++) {
+			Point2		center = new Point2(rnd.nextDouble() * 100.0 - 50.0, rnd.nextDouble() * 100.0 - 50.0);
+			double		radius = rnd.nextDouble() * 10.0;
+			Circle		circle = new Circle(center, radius);
+			Point2		origin = new Point2(rnd.nextDouble() * 100.0 - 50.0, rnd.nextDouble() * 100.0 - 50.0);
+			double		theta = rnd.nextDouble() * 2.0 * Math.PI;
+			double		r = rnd.nextDouble() * radius;
+			Vector2		centerToTarget = new Vector2(r * Math.cos(theta), r * Math.sin(theta));
+			Point2		target = center.plus(centerToTarget);
+			Vector2		direction = target.vectorFrom(origin).unit();
+			Ray2		ray2 = new Ray2(origin, direction);
+			Interval	J = circle.intersect(ray2);
+			Point2		p0 = ray2.pointAt(J.getMinimum());
+			Point2		p1 = ray2.pointAt(J.getMaximum());
+
+			boolean		ok0 = circle.nearBoundary(p0) || circle.contains(p0);
+			boolean		ok1 = circle.nearBoundary(p1);
+
+			if (!ok0 || !ok1) {
+				System.out.println("Error, ray-circle intersection point not on boundary of circle.");
+				passed = false;
+				break;
+			}
+		}
+
+		if (passed) {
+			System.out.println("ray-circle intersection test passed.");
+		}
 
 	}
 
