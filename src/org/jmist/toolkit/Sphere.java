4ad8c896ede6dbcf14a45537ac1fb7b1ca35e9c4
Brad Kimmel <intrepidca@gmail.com>
2007-06-04 01:56:24 +0000

    Added Sphere.intersect to compute the intersection of a ray with a sphere. Added Sphere.intersects to determine if a ray intersects a sphere.

--- src/org/jmist/toolkit/Sphere.java
+++ src/org/jmist/toolkit/Sphere.java
@@ -107,6 +107,99 @@ public final class Sphere {
 			}
 		}
 	}
+	
+	/**
+	 * Determines whether the specified ray intersects with
+	 * this sphere.  Equivalent to {@code !this.intersect(ray).isEmpty()}.
+	 * @param ray The ray to test for an intersection with.
+	 * @return A value indicating whether the ray intersects
+	 * 		this sphere.
+	 * @see {@link #intersect(Ray3)}, {@link Interval#isEmpty()}.
+	 */
+	public boolean intersects(Ray3 ray) {
+		
+		//
+		// Algorithm from:
+		//
+		// A.S. Glassner, Ed.,
+		// "An Introduction to Ray Tracing",
+		// Morgan Kaufmann Publishers, Inc., San Francisco, CA, 2002
+		// Section 2.2
+		//
+
+		if (isEmpty()) {
+			return false;
+		}
+		
+		double		r2 = radius * radius;
+		Vector3		oc = ray.getOrigin().vectorTo(center);
+		double		L2oc = oc.dot(oc);
+		
+		if (L2oc < r2) return true;
+		
+		double		tca = oc.dot(ray.getDirection());
+		
+		if (tca < 0.0) return false;
+		
+		return ray.pointAt(tca).squaredDistanceTo(center) < r2;
+		
+	}
+
+	/**
+	 * Computes the intersection of this sphere with the given
+	 * ray.
+	 * @param ray The ray to compute the intersection of this
+	 * 		sphere with.
+	 * @return The interval in which the ray passes through
+	 * 		the sphere (i.e., this.contains(ray.pointAt(x)) if and
+	 * 		only if this.intersect(ray).contains(x)).
+	 * @see {@link #contains(Point3)}, {@link Ray3#pointAt(double)},
+	 * 		{@link Interval#contains(double)}.
+	 */
+	public Interval intersect(Ray3 ray) {
+		
+		//
+		// Algorithm from:
+		//
+		// A.S. Glassner, Ed.,
+		// "An Introduction to Ray Tracing",
+		// Morgan Kaufmann Publishers, Inc., San Francisco, CA, 2002
+		// Section 2.2
+		//
+
+		// Check for an empty box.
+		if (isEmpty()) {
+			return Interval.EMPTY;
+		}
+
+		// Check if the ray starts from within the box.
+		double		r2 = radius * radius;
+		Vector3		oc = ray.getOrigin().vectorTo(center);
+		double		L2oc = oc.dot(oc);
+		boolean		startInside = (L2oc < r2);
+
+		// distance along ray to point on ray closest to center of sphere (equation (A10)).
+		double		tca = oc.dot(ray.getDirection());
+		
+		// if the ray starts outside the sphere and points away from the center of the
+		// sphwere, then the ray does not hit the sphere.
+		if (!startInside && tca < 0.0) {
+			return Interval.EMPTY;
+		}
+		
+		// compute half chord distance squared (equation (A13)).
+		double		t2hc = r2 - L2oc + (tca * tca);
+		
+		if (t2hc < 0.0) {
+			return Interval.EMPTY;
+		}
+		
+		double		thc = Math.sqrt(t2hc);
+		
+		// compute interval (equation (A14)).
+		return new Interval(startInside ? 0.0 : (tca - thc), tca + thc);
+
+	}
 
 	/**
 	 * Default constructor.
