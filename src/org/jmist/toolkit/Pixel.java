06fc20daf45489186130044ed13642651a44c2b0
Brad Kimmel <intrepidca@gmail.com>
2007-08-12 20:27:33 +0000

    Added stubs for Pixel.add, Pixel.scale, and Pixel.reset.

--- src/org/jmist/toolkit/Pixel.java
+++ src/org/jmist/toolkit/Pixel.java
@@ -5,8 +5,20 @@ package org.jmist.toolkit;
 
 /**
  * @author bkimmel
- *
+ * TODO: implement this class.
  */
 public final class Pixel {
+
+	public void reset() {
+		// TODO: implement this method.
+	}
+
+	public void add(Pixel pixel) {
+		// TODO: implement this method.
+	}
+
+	public void scale(double factor) {
+		// TODO: implement this method.
+	}
 
 }
