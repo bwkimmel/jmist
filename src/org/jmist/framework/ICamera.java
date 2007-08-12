ba872de7fac629ce8c27884b58b3a77b68c033dd
Brad Kimmel <intrepidca@gmail.com>
2007-08-12 19:55:32 +0000

    Changed responsibility of ICamera:  ICamera will now be responsible for rendering a point on the image plane.  It will have a single method shadeAt(Point2, Pixel) that will compute the responses for the given point on the image plane.
    ILens will have the responsibility previously delegated to ICamera (i.e.,
    generating a ray corresponding to a given point on the image plane).

--- src/org/jmist/framework/ICamera.java
+++ src/org/jmist/framework/ICamera.java
@@ -6,24 +6,30 @@ package org.jmist.framework;
 import org.jmist.toolkit.*;
 
 /**
+ * Computes the colour channel responses for points on the image
+ * plane.
  * @author bkimmel
- *
  */
-public interface ICamera {
+public interface ICamera extends IPixelFactory {
 
 	/**
-	 * Gets a ray indicating from which point and
-	 * direction the camera is sensitive to incoming
-	 * light at the specified point on its image plane.
-	 * This will correspond to the direction to cast
-	 * a ray in order to shade the specified point on
-	 * the image plane.
-	 * @param p The point on the image plane in
-	 * 		normalized device coordinates (must fall
-	 * 		within {@code Box2.UNIT}).
-	 * @return The ray to cast for ray shading.
-	 * @see Box2#UNIT
+	 * Obtains the camera colour channel responses at a
+	 * specified point on the normalized image plane.
+	 * @param p The point on the image plane in normalized
+	 * 		device coordinates (must fall within
+	 * 		{@code Box2.UNIT}).
+	 * @param pixel The camera colour channel responses
+	 * 		at the specified point on the image plane.  The pixel
+	 * 		must have been created using {@code this.createPixel()}.
+	 * 		The values of the elements of responses shall not
+	 * 		depend on their initial values.  Note that this
+	 * 		requirement implies that this method is responsible
+	 * 		for initializing the values of the pixel.  In
+	 * 		particular, the implementation of this method must
+	 * 		not assume that the elements have been set to zero
+	 * 		prior to invocation.
+	 * @see {@link Box2#UNIT}, {@link IPixelFactory#createPixel()}.
 	 */
-	Ray3 rayAt(Point2 p);
+	void shadeAt(Point2 p, Pixel pixel);
 
 }
---
+++ src/org/jmist/framework/ILens.java
@@ -0,0 +1,30 @@
+/**
+ *
+ */
+package org.jmist.framework;
+
+import org.jmist.toolkit.*;
+
+/**
+ * Generates the ray to cast corresponding to given points on the
+ * image plane.
+ * @author bkimmel
+ */
+public interface ILens {
+
+	/**
+	 * Gets a ray indicating from which point and
+	 * direction the camera is sensitive to incoming
+	 * light at the specified point on its image plane.
+	 * This will correspond to the direction to cast
+	 * a ray in order to shade the specified point on
+	 * the image plane.
+	 * @param p The point on the image plane in
+	 * 		normalized device coordinates (must fall
+	 * 		within {@code Box2.UNIT}).
+	 * @return The ray to cast for ray shading.
+	 * @see {@link Box2#UNIT}
+	 */
+	Ray3 rayAt(Point2 p);
+
+}
