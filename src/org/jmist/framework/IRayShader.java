f6a4e59ffc271448db11539b3c08605d68227711
Brad Kimmel <intrepidca@gmail.com>
2007-08-12 19:58:34 +0000

    Changed IRayShader from spectral-based to pixel-based.

--- src/org/jmist/framework/IRayShader.java
+++ src/org/jmist/framework/IRayShader.java
@@ -3,25 +3,23 @@
  */
 package org.jmist.framework;
 
-import org.jmist.toolkit.*;
+import org.jmist.toolkit.Pixel;
+import org.jmist.toolkit.Ray3;
 
 /**
+ * Estimates the colour channel responses along a given ray.
  * @author bkimmel
- *
  */
-public interface IRayShader {
+public interface IRayShader extends IPixelFactory {
 
 	/**
-	 * Computes an estimate of the spectral radiance at the origin of
-	 * the ray travelling in the direction opposite the direction of
+	 * Computes an estimate of the colour channel responses at the origin
+	 * of the ray travelling in the direction opposite the direction of
 	 * the ray.
-	 * @param ray The ray indicating the point and direction along
-	 * 		which to compute the spectral radiance.
-	 * @param wavelength The wavelength (in meters) at which to
-	 * 		evaluate the spectral radiance.
-	 * @return The spectral radiance (in
-	 * 		W&middot;sr<sup>-1</sup>&middot;m<sup>-3</sup>).
+	 * @param ray The ray indicating the point and direction along which to
+	 * 		compute the colour channel responses.
+	 * @param pixel The colour channel responses.
 	 */
-	double shadeRay(Ray3 ray, double wavelength);
+	void shadeRay(Ray3 ray, Pixel pixel);
 
 }
