d5322dca0fce261e6fed61660411293324dba1eb
Brad Kimmel <intrepidca@gmail.com>
2007-10-14 17:20:29 +0000

    Refactoring IModelElement.

--- src/org/jmist/framework/model/IModelElement.java
+++ src/org/jmist/framework/model/IModelElement.java
@@ -15,16 +15,36 @@ import org.jmist.toolkit.*;
  */
 public interface IModelElement extends IVisibilityFunction3, IBounded3 {
 
-	String getName();
-
 	boolean contains(Point3 p);
 
 	ISurfacePoint generateRandomSurfacePoint();
 
-	void addChild(IModelElement child) throws UnsupportedOperationException;
-	void removeChild(String name);
-	IModelElement findChild(String name);
+	
 
+	/*
+	 * The following methods maintain the list of children of this
+	 * model element.
+	 */
+
+	/**
+	 * Adds a child model element.
+	 * @param child The child to add.
+	 * @throws UnsupportedOperationException If this model element is not
+	 * 		a composite model element.
+	 */
+	void addChild(IModelElement child) throws UnsupportedOperationException;
+
+	/**
+	 * Removes a child model element.  This method has no effect if {@code child}
+	 * is not a child of this element.
+	 * @param child The child to remove.
+	 */
+	void removeChild(IModelElement child);
+
+	/**
+	 * Gets the collection of children of this model element.
+	 * @return The collection of children of this model element.
+	 */
 	Iterable<IModelElement> children();
 
 }
