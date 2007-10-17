8d332dc644ec52e865201439e0616dfdcfaf79f4
Brad Kimmel <intrepidca@gmail.com>
2007-10-17 02:57:21 +0000

    Added solver for roots of the quartic polynomial.

--- src/org/jmist/util/Solver.java
+++ src/org/jmist/util/Solver.java
@@ -33,6 +33,9 @@ public final class Solver {
 			case 4:		/* cubic */
 				return roots(c[0], c[1], c[2], c[3]);
 
+			case 5:		/* quartic */
+				return roots(c[0], c[1], c[2], c[3], c[4]);
+
 			default:
 				// TODO: handle higher order polynomials.
 				assert(false);
@@ -187,6 +190,143 @@ public final class Solver {
 			}
 
 		}
+
+	}
+
+	/**
+	 * Computes the real roots of the quartic equation:
+	 * 0 = c0 + (c1 * x) + (c2 * x^2) + (c3 * x^3) + (c4 * x^4)
+	 * @param c0	the coefficient to x^0
+	 * @param c1	the coefficient to x^1
+	 * @param c2	the coefficient to x^2
+	 * @param c3	the coefficient to x^3
+	 * @param c4	the coefficient to x^4
+	 * @return An array containing the real roots of the quartic
+	 * 		equation with the given coefficients.
+	 */
+	public static double[] roots(double c0, double c1, double c2, double c3, double c4) {
+
+		/*
+		 * The following is adapted from:
+		 *
+		 * Glassner, A., "Graphics Gems", Academic Press, 1995.  (Appendix 2)
+		 */
+
+	    double[]	coeffs			= new double[4];
+	    double[]	result;
+	    double[]	s, t;
+	    double		z, u, v, sub;
+	    double		A, B, C, D;
+	    double		sq_A, p, q, r;
+	    int			i;
+
+	    /* normal form: x^4 + Ax^3 + Bx^2 + Cx + D = 0 */
+
+	    A = c3 / c4;
+	    B = c2 / c4;
+	    C = c1 / c4;
+	    D = c0 / c4;
+
+	    /*  substitute x = y - A/4 to eliminate cubic term:
+		x^4 + px^2 + qx + r = 0 */
+
+	    sq_A = A * A;
+	    p = - 3.0/8 * sq_A + B;
+	    q = 1.0/8 * sq_A * A - 1.0/2 * A * B + C;
+	    r = - 3.0/256*sq_A*sq_A + 1.0/16*sq_A*B - 1.0/4*A*C + D;
+
+	    if (isZero(r)) {
+
+			/* no absolute term: y(y^3 + py + q) = 0 */
+
+			s = roots(q, p, 0.0, 1.0);
+
+			result = new double[s.length + 1];
+			for (i = 0; i < s.length; i++) {
+				result[i] = s[i];
+			}
+
+			result[i] = 0;
+
+	    } else {
+
+			/* solve the resolvent cubic ... */
+
+			coeffs[ 0 ] = 1.0/2 * r * p - 1.0/8 * q * q;
+			coeffs[ 1 ] = - r;
+			coeffs[ 2 ] = - 1.0/2 * p;
+			coeffs[ 3 ] = 1;
+
+			s = roots(coeffs);
+
+			/* ... and take the one real solution ... */
+
+			z = s[ 0 ];
+
+			/* ... to build two quadric equations */
+
+			u = z * z - r;
+			v = 2 * z - p;
+
+			if (isZero(u))
+			    u = 0;
+			else if (u > 0)
+			    u = Math.sqrt(u);
+			else
+			    return new double[0];
+
+			if (isZero(v))
+			    v = 0;
+			else if (v > 0)
+			    v = Math.sqrt(v);
+			else
+			    return new double[0];
+
+			coeffs[ 0 ] = z - u;
+			coeffs[ 1 ] = q < 0 ? -v : v;
+			coeffs[ 2 ] = 1;
+
+			s = roots(coeffs);
+
+			coeffs[ 0 ]= z + u;
+			coeffs[ 1 ] = q < 0 ? v : -v;
+			coeffs[ 2 ] = 1;
+
+			t = roots(coeffs);
+
+			result = new double[s.length + t.length];
+
+			for (i = 0; i < s.length; i++) {
+				result[i] = s[i];
+			}
+
+			for (i = 0; i < t.length; i++) {
+				result[i + s.length] = t[i];
+			}
+
+	    }
+
+	    /* resubstitute */
+
+	    sub = 1.0/4 * A;
+
+	    for (i = 0; i < result.length; ++i)
+	    	result[ i ] -= sub;
+
+	    return result;
+
+	}
+
+	/**
+	 * Determines if the specified floating point value is close to zero.
+	 * @param value The value to compare with zero.
+	 * @return A value that indicates whether {@code value} is near zero.
+	 */
+	private static boolean isZero(double value) {
+
+		final double eps = Double.longBitsToDouble(Double.doubleToRawLongBits(1.0) + 1) - 1.0;
+
+		return Math.abs(value) < eps;
 
 	}
 
