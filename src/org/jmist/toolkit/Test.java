ed9a33224933f8a93cef42ebbe3793c3005614a4
Brad Kimmel <intrepidca@gmail.com>
2007-10-21 04:03:43 +0000

    Put individual tests in separate methods.

--- src/org/jmist/toolkit/Test.java
+++ src/org/jmist/toolkit/Test.java
@@ -100,98 +100,134 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				System.out.print(" " + LinearMatrix3.IDENTITY.at(i, j));
+		testLinearMatrix3Identity();
+		testReflect();
+		testWeakReference();
+		testObserver();
+		testTuple();
+		testGrid();
+		testRayCircleIntersection();
+		testSwap();
+		testPermutations();
+		testNRooks();
+		testPolynomial();
+		testRoots();
+
+	}
+
+	/**
+	 *
+	 */
+	private static void testRoots() {
+		Polynomial[] roots = new Polynomial[4];
+		Polynomial[] poly = new Polynomial[4];
+		int i;
+		java.util.Random rnd = new java.util.Random();
+
+		System.out.print("All roots:");
+		for (i = 0; i < roots.length; i++) {
+			roots[i] = new Polynomial(-rnd.nextDouble(), 1.0);
+			System.out.print(" ");
+			System.out.print(-roots[i].coefficient(0));
+			poly[i] = i > 0 ? poly[i - 1].times(roots[i]) : roots[i];
+		}
+		System.out.println();
+
+		for (i = 0; i < poly.length; i++) {
+			System.out.println(poly[i]);
+			System.out.print("roots:");
+			for (double root : poly[i].roots()) {
+				System.out.print(" ");
+				System.out.print(root);
+			}
+			System.out.println();
+		}
+	}
+
+	/**
+	 *
+	 */
+	private static void testPolynomial() {
+		Polynomial p = new Polynomial(3, 2, 1);
+		Polynomial q = new Polynomial(-4, 1, -1);
+
+		Polynomial r = p.times(q);
+		System.out.printf("(%s)(%s) = (%s)\n", p.toString(), q.toString(), r.toString());
+
+		r = p.plus(q);
+		System.out.printf("(%s) + (%s) = (%s)", p.toString(), q.toString(), r.toString());
+
+		System.out.println("------------------------");
+	}
+
+	/**
+	 *
+	 */
+	private static void testNRooks() {
+		NRooksRandom rand = new NRooksRandom(10, 4);
+		int i;
+		int j;
+		for (i = 0; i < 10; i++) {
+			for (j = 0; j < 4; j++) {
+				if (j > 0) {
+					System.out.print(", ");
+				}
+				System.out.printf("%f", rand.next());
 			}
 			System.out.println();
 		}
 
-		Vector3 I = new Vector3(1.0, 1.0, 1.0).unit();
-		Vector3 R = Optics.reflect(I, Vector3.J);
+		System.out.println("------------------------");
+	}
 
-		Object x = new Integer(5);
-		Reference<Object> ref1 = new WeakReference<Object>(x);
-		Reference<Object> ref2 = new WeakReference<Object>(x);
+	/**
+	 *
+	 */
+	private static void testPermutations() {
+		Permutation Px = Permutation.random(8);
+		Permutation Py = Permutation.random(8);
 
-		System.out.println(ref1.get().equals(ref2.get()));
+		int board[][] = new int[8][8];
+		int i,j;
 
-		TestSubject subject = new TestSubject();
-		TestObserver observer = new TestObserver(subject);
+		for (i = 0; i < 8; i++) {
+			board[Px.at(i)][Py.at(i)] = i + 1;
+		}
 
-		subject.raiseEventA(5);
-		observer.print();
-		observer.disableEventA();
-		subject.raiseEventA(4);
-		observer.print();
-		observer.enableEventA();
-		subject.raiseEventA(3);
-		observer.print();
-		subject.raiseEventB();
-		observer.disableEventB();
-		subject.raiseEventB();
-		observer.enableEventB();
-		subject.raiseEventB();
-
-		double[] array = new double[]{ 1.0, 2.0, 3.0 };
-		Tuple zzz = new Tuple(array);
-		array[1] = -2.0;
-
-		System.out.println("-------");
-		System.out.println(zzz.filter(new Tuple.Filter() { public boolean apply(double value) { return value < 2.5; } }));
-		System.out.println(zzz.map(new Tuple.Function() {
-
-			public double apply(double value) {
-				return value * value;
-			}} ));
-		System.out.println(zzz.reduce(Tuple.PRODUCT_OPERATOR));
-
-		System.out.println(zzz.reverse());
-		System.out.println(zzz.append(zzz.reverse()));
-		System.out.println(zzz.append(4.0));
-		System.out.println(zzz.append(new double[]{4.0, 5.0}));
-		System.out.println(zzz.permute(new int[]{2,1,2,1,0}));
-		System.out.println(zzz.right(2));
-		System.out.println(zzz.left(2));
-		System.out.println(zzz.append(zzz.reverse()).slice(2, 5));
-		System.out.println(Tuple.zeros(5));
-		System.out.println(Tuple.ones(7));
-		System.out.println(zzz.combine(Tuple.ones(3), Tuple.SUM_OPERATOR));
-				System.out.println("-------");
-		System.out.print("zzz[1]=");
-		System.out.println(zzz.at(1));
-
-		Grid3 grid = new Grid3(new Box3(0.0, 0.0, 0.0, 1.0, 1.0, 1.0), 4, 4, 4);
-		Ray3 ray = new Ray3(new Point3(-1.0, -1.5, -1.0), new Vector3(1.4, 2.1, 1.3).unit());
-
-		Grid3.IVisitor visitor = new Grid3.IVisitor() {
-
-			/* (non-Javadoc)
-			 * @see org.jmist.toolkit.Grid3.IVisitor#visit(org.jmist.toolkit.Ray3, org.jmist.toolkit.Interval, org.jmist.toolkit.Grid3.Cell)
-			 */
-			public boolean visit(Ray3 ray, Interval I, Cell cell) {
-
-				System.out.println("I=[" + I.minimum() + ", " + I.maximum() + "]");
-				System.out.println("cell=[" + cell.getX() + " " + cell.getY() + " " + cell.getZ() + "]");
-
-				Point3		p0 = ray.pointAt(I.minimum());
-				Point3		p1 = ray.pointAt(I.maximum());
-				Vector3		N0 = cell.getBoundingBox().normalAt(p0);
-				Vector3		N1 = cell.getBoundingBox().normalAt(p1);
-
-				System.out.println("p0=(" + p0.x() + ", " + p0.y() + ", " + p0.z() + "); N0=(" + N0.x() + ", " + N0.y() + ", " + N0.z() + ")");
-				System.out.println("p1=(" + p1.x() + ", " + p1.y() + ", " + p1.z() + "); N1=(" + N1.x() + ", " + N1.y() + ", " + N1.z() + ")");
-
-				System.out.println("-----------------------------");
-
-				return true;
-
+		for (i = 0; i < 8; i++) {
+			for (j = 0; j < 8; j++) {
+				if (board[j][i] > 0) {
+					System.out.printf("[%d]", board[j][i]);
+				} else {
+					System.out.print("[ ]");
+				}
 			}
+			System.out.println();
+		}
 
-		};
+		System.out.println("------------------------");
+	}
 
-		grid.intersect(ray, Interval.POSITIVE, visitor);
+	/**
+	 *
+	 */
+	private static void testSwap() {
+		double[] arr = new double[2];
 
+		arr[0] = 1.0;
+		arr[1] = 2.0;
+
+		System.out.println("arr=[" + arr[0] + ", " + arr[1] + "]");
+		swapFirstTwoElements(arr);
+		System.out.println("arr=[" + arr[0] + ", " + arr[1] + "]");
+
+		System.out.println("------------------------");
+	}
+
+	/**
+	 * @return
+	 */
+	private static void testRayCircleIntersection() {
 		boolean passed = true;
 		java.util.Random rnd = new java.util.Random();
 
@@ -224,85 +260,128 @@ public class Test {
 			System.out.println("ray-circle intersection test passed.");
 		}
 
-		double[] arr = new double[2];
+	}
 
-		arr[0] = 1.0;
-		arr[1] = 2.0;
+	/**
+	 *
+	 */
+	private static void testGrid() {
+		Grid3 grid = new Grid3(new Box3(0.0, 0.0, 0.0, 1.0, 1.0, 1.0), 4, 4, 4);
+		Ray3 ray = new Ray3(new Point3(-1.0, -1.5, -1.0), new Vector3(1.4, 2.1, 1.3).unit());
 
-		System.out.println("arr=[" + arr[0] + ", " + arr[1] + "]");
-		swapFirstTwoElements(arr);
-		System.out.println("arr=[" + arr[0] + ", " + arr[1] + "]");
+		Grid3.IVisitor visitor = new Grid3.IVisitor() {
 
-		System.out.println("------------------------");
+			/* (non-Javadoc)
+			 * @see org.jmist.toolkit.Grid3.IVisitor#visit(org.jmist.toolkit.Ray3, org.jmist.toolkit.Interval, org.jmist.toolkit.Grid3.Cell)
+			 */
+			public boolean visit(Ray3 ray, Interval I, Cell cell) {
 
-		Permutation Px = Permutation.random(8);
-		Permutation Py = Permutation.random(8);
+				System.out.println("I=[" + I.minimum() + ", " + I.maximum() + "]");
+				System.out.println("cell=[" + cell.getX() + " " + cell.getY() + " " + cell.getZ() + "]");
 
-		int board[][] = new int[8][8];
-		int i,j;
+				Point3		p0 = ray.pointAt(I.minimum());
+				Point3		p1 = ray.pointAt(I.maximum());
+				Vector3		N0 = cell.getBoundingBox().normalAt(p0);
+				Vector3		N1 = cell.getBoundingBox().normalAt(p1);
 
-		for (i = 0; i < 8; i++) {
-			board[Px.at(i)][Py.at(i)] = i + 1;
-		}
+				System.out.println("p0=(" + p0.x() + ", " + p0.y() + ", " + p0.z() + "); N0=(" + N0.x() + ", " + N0.y() + ", " + N0.z() + ")");
+				System.out.println("p1=(" + p1.x() + ", " + p1.y() + ", " + p1.z() + "); N1=(" + N1.x() + ", " + N1.y() + ", " + N1.z() + ")");
 
-		for (i = 0; i < 8; i++) {
-			for (j = 0; j < 8; j++) {
-				if (board[j][i] > 0) {
-					System.out.printf("[%d]", board[j][i]);
-				} else {
-					System.out.print("[ ]");
-				}
+				System.out.println("-----------------------------");
+
+				return true;
+
+			}
+
+		};
+
+		grid.intersect(ray, Interval.POSITIVE, visitor);
+	}
+
+	/**
+	 *
+	 */
+	private static void testTuple() {
+		double[] array = new double[]{ 1.0, 2.0, 3.0 };
+		Tuple zzz = new Tuple(array);
+		array[1] = -2.0;
+
+		System.out.println("-------");
+		System.out.println(zzz.filter(new Tuple.Filter() { public boolean apply(double value) { return value < 2.5; } }));
+		System.out.println(zzz.map(new Tuple.Function() {
+
+			public double apply(double value) {
+				return value * value;
+			}} ));
+		System.out.println(zzz.reduce(Tuple.PRODUCT_OPERATOR));
+
+		System.out.println(zzz.reverse());
+		System.out.println(zzz.append(zzz.reverse()));
+		System.out.println(zzz.append(4.0));
+		System.out.println(zzz.append(new double[]{4.0, 5.0}));
+		System.out.println(zzz.permute(new int[]{2,1,2,1,0}));
+		System.out.println(zzz.right(2));
+		System.out.println(zzz.left(2));
+		System.out.println(zzz.append(zzz.reverse()).slice(2, 5));
+		System.out.println(Tuple.zeros(5));
+		System.out.println(Tuple.ones(7));
+		System.out.println(zzz.combine(Tuple.ones(3), Tuple.SUM_OPERATOR));
+				System.out.println("-------");
+		System.out.print("zzz[1]=");
+		System.out.println(zzz.at(1));
+	}
+
+	/**
+	 *
+	 */
+	private static void testObserver() {
+		TestSubject subject = new TestSubject();
+		TestObserver observer = new TestObserver(subject);
+
+		subject.raiseEventA(5);
+		observer.print();
+		observer.disableEventA();
+		subject.raiseEventA(4);
+		observer.print();
+		observer.enableEventA();
+		subject.raiseEventA(3);
+		observer.print();
+		subject.raiseEventB();
+		observer.disableEventB();
+		subject.raiseEventB();
+		observer.enableEventB();
+		subject.raiseEventB();
+	}
+
+	/**
+	 *
+	 */
+	private static void testReflect() {
+		Vector3 I = new Vector3(1.0, 1.0, 1.0).unit();
+		Vector3 R = Optics.reflect(I, Vector3.J);
+	}
+
+	/**
+	 *
+	 */
+	private static void testWeakReference() {
+		Object x = new Integer(5);
+		Reference<Object> ref1 = new WeakReference<Object>(x);
+		Reference<Object> ref2 = new WeakReference<Object>(x);
+
+		System.out.println(ref1.get().equals(ref2.get()));
+	}
+
+	/**
+	 *
+	 */
+	private static void testLinearMatrix3Identity() {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print(" " + LinearMatrix3.IDENTITY.at(i, j));
 			}
 			System.out.println();
 		}
-
-		System.out.println("------------------------");
-		NRooksRandom rand = new NRooksRandom(10, 4);
-
-		for (i = 0; i < 10; i++) {
-			for (j = 0; j < 4; j++) {
-				if (j > 0) {
-					System.out.print(", ");
-				}
-				System.out.printf("%f", rand.next());
-			}
-			System.out.println();
-		}
-
-		System.out.println("------------------------");
-		Polynomial p = new Polynomial(3, 2, 1);
-		Polynomial q = new Polynomial(-4, 1, -1);
-
-		Polynomial r = p.times(q);
-		System.out.printf("(%s)(%s) = (%s)\n", p.toString(), q.toString(), r.toString());
-
-		r = p.plus(q);
-		System.out.printf("(%s) + (%s) = (%s)", p.toString(), q.toString(), r.toString());
-
-		System.out.println("------------------------");
-
-		Polynomial[] roots = new Polynomial[4];
-		Polynomial[] poly = new Polynomial[4];
-
-		System.out.print("All roots:");
-		for (i = 0; i < roots.length; i++) {
-			roots[i] = new Polynomial(-rnd.nextDouble(), 1.0);
-			System.out.print(" ");
-			System.out.print(-roots[i].coefficient(0));
-			poly[i] = i > 0 ? poly[i - 1].times(roots[i]) : roots[i];
-		}
-		System.out.println();
-
-		for (i = 0; i < poly.length; i++) {
-			System.out.println(poly[i]);
-			System.out.print("roots:");
-			for (double root : poly[i].roots()) {
-				System.out.print(" ");
-				System.out.print(root);
-			}
-			System.out.println();
-		}
-
 	}
 
 	public static void swapFirstTwoElements(double[] arr) {
