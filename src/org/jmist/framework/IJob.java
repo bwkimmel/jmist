2064a0860665d7646818eb1413319f99f4e4e323
Brad Kimmel <intrepidca@gmail.com>
2007-10-21 23:41:35 +0000

    Added javadoc comments to IJob.

--- src/org/jmist/framework/IJob.java
+++ src/org/jmist/framework/IJob.java
@@ -4,11 +4,16 @@
 package org.jmist.framework;
 
 /**
+ * Represents a job.
  * @author bkimmel
- *
  */
 public interface IJob {
 
+	/**
+	 * Runs the job.
+	 * @param monitor The <code>IProgressMonitor</code> to report the progress
+	 * 		of the job to.
+	 */
 	void go(IProgressMonitor monitor);
 
 }
