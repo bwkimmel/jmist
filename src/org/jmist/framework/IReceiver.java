fb77e668045e344e6fc69af6ed0d0bb856703752
Brad Kimmel <intrepidca@gmail.com>
2007-10-22 19:52:16 +0000

    Removed communication methods that have a timeout parameter.

--- src/org/jmist/framework/IReceiver.java
+++ src/org/jmist/framework/IReceiver.java
@@ -11,15 +11,6 @@ public interface IReceiver {
 
 	/**
 	 * Wait for an incoming message.
-	 * @param timeout The length of time (in milliseconds) to wait before
-	 * 		timing out.
-	 * @return The incoming message, or <code>null</code> if no message was
-	 * 		received before <code>timeout</code> milliseconds elapsed.
-	 */
-	IInboundMessage receive(long timeout);
-
-	/**
-	 * Wait for an incoming message.
 	 * @return The incoming message.
 	 */
 	IInboundMessage receive();
--- src/org/jmist/framework/ISwitchboard.java
+++ src/org/jmist/framework/ISwitchboard.java
@@ -11,15 +11,6 @@ public interface ISwitchboard extends IDialer {
 
 	/**
 	 * Wait for an incoming connection or message.
-	 * @param timeout The length of time (in milliseconds) to wait.
-	 * @return The new connection or the connection on which an incoming
-	 * 		message was received, or <code>null</code> if <code>timeout</code>
-	 * 		milliseconds elapsed before any activity occurred.
-	 */
-	ICommunicator listen(long timeout);
-
-	/**
-	 * Wait for an incoming connection or message.
 	 * @return The new connection or the connection on which an incoming
 	 * 		message was received.
 	 */
