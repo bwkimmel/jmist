dc44ba8ac6ea9cd541f058f44b1ac448902edf2e
Brad Kimmel <intrepidca@gmail.com>
2007-10-27 00:57:59 +0000

    Added job to submit a parallelizable job to a JobMasterService.

---
+++ src/org/jmist/framework/services/ServiceSubmitJob.java
@@ -0,0 +1,59 @@
+/**
+ *
+ */
+package org.jmist.framework.services;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.UUID;
+
+import org.jmist.framework.IJob;
+import org.jmist.framework.IParallelizableJob;
+import org.jmist.framework.IProgressMonitor;
+
+/**
+ * @author bkimmel
+ *
+ */
+public final class ServiceSubmitJob implements IJob {
+
+	/* (non-Javadoc)
+	 * @see org.jmist.framework.IJob#go(org.jmist.framework.IProgressMonitor)
+	 */
+	@Override
+	public void go(IProgressMonitor monitor) {
+
+		try {
+
+			monitor.notifyStatusChanged("Submitting job...");
+
+			Registry registry = LocateRegistry.getRegistry(this.masterHost);
+			IJobMasterService service = (IJobMasterService) registry.lookup("IJobMasterService");
+			UUID jobId = service.submitJob(this.job, this.priority);
+
+			if (jobId.compareTo(new UUID(0, 0)) != 0) {
+
+				monitor.notifyStatusChanged(String.format("Submitted, ID=%s.", jobId.toString()));
+				monitor.notifyComplete();
+
+			} else {
+
+				monitor.notifyStatusChanged("Failed to submit job.");
+				monitor.notifyCancelled();
+
+			}
+
+		} catch (Exception e) {
+
+			System.err.println("Client exception: " + e.toString());
+			e.printStackTrace();
+
+		}
+
+	}
+
+	private String masterHost;
+	private IParallelizableJob job;
+	private int priority;
+
+}
