bc18e0eac03f5eac7b04b126cb8334f5f39ebfc9
Brad Kimmel <intrepidca@gmail.com>
2007-10-27 01:46:09 +0000

    - Added javadoc comments. - Added constructor.

--- src/org/jmist/framework/services/ServiceSubmitJob.java
+++ src/org/jmist/framework/services/ServiceSubmitJob.java
@@ -12,10 +12,25 @@ import org.jmist.framework.IParallelizableJob;
 import org.jmist.framework.IProgressMonitor;
 
 /**
+ * A job that submits an <code>IParallelizableJob</code> to submit to a remote
+ * <code>JobMasterService</code>.
  * @author bkimmel
- *
  */
 public final class ServiceSubmitJob implements IJob {
+
+	/**
+	 * Initializes the job to submit, the priority to assign the job, and the
+	 * URL of the master to submit the job to.
+	 * @param job The <code>IParallelizableJob</code> to submit.
+	 * @param priority The priority to assign the job.
+	 * @param masterHost The URL of the remote <code>JobMasterService</code> to
+	 * 		submit the job to.
+	 */
+	public ServiceSubmitJob(IParallelizableJob job, int priority, String masterHost) {
+		this.job = job;
+		this.priority = priority;
+		this.masterHost = masterHost;
+	}
 
 	/* (non-Javadoc)
 	 * @see org.jmist.framework.IJob#go(org.jmist.framework.IProgressMonitor)
@@ -56,8 +71,16 @@ public final class ServiceSubmitJob implements IJob {
 
 	}
 
-	private String masterHost;
-	private IParallelizableJob job;
-	private int priority;
+	/**
+	 * The URL of the remote <code>JobMasterService</code> to submit the job
+	 * to.
+	 */
+	private final String masterHost;
+
+	/** The <code>IParallelizableJob</code> to submit. */
+	private final IParallelizableJob job;
+
+	/** The priority to assign to the job. */
+	private final int priority;
 
 }
