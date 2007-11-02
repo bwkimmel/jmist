fad25ea2f1a505a48a5f8e3bba3d8de776df9502
Brad Kimmel <intrepidca@gmail.com>
2007-11-02 03:08:12 +0000

    Added absract material for opaque materials (no transmission) (OpaqueMaterial).

---
+++ src/org/jmist/framework/OpaqueMaterial.java
@@ -0,0 +1,39 @@
+/**
+ *
+ */
+package org.jmist.framework;
+
+import org.jmist.toolkit.Point3;
+import org.jmist.toolkit.Ray3;
+
+/**
+ * @author bkimmel
+ *
+ */
+public abstract class OpaqueMaterial implements Material {
+
+	/* (non-Javadoc)
+	 * @see org.jmist.framework.Medium#extinctionIndex(org.jmist.toolkit.Point3)
+	 */
+	@Override
+	public Spectrum extinctionIndex(Point3 p) {
+		return Spectrum.POSITIVE_INFINITY;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jmist.framework.Medium#refractiveIndex(org.jmist.toolkit.Point3)
+	 */
+	@Override
+	public Spectrum refractiveIndex(Point3 p) {
+		return Spectrum.ONE;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jmist.framework.Medium#transmittance(org.jmist.toolkit.Ray3, double)
+	 */
+	@Override
+	public Spectrum transmittance(Ray3 ray, double distance) {
+		return Spectrum.ZERO;
+	}
+
+}
