fa002f7fb1c4b0bb767dacee475ce338c4fe3054
Brad Kimmel <intrepidca@gmail.com>
2007-11-03 04:31:39 +0000

    Initial import of stubs for some concrete implementations of CollectorSphere.

---
+++ src/org/jmist/packages/EqualSolidAnglesCollectorSphere.java
@@ -0,0 +1,61 @@
+/**
+ *
+ */
+package org.jmist.packages;
+
+import org.jmist.framework.measurement.AbstractCollectorSphere;
+import org.jmist.framework.measurement.CollectorSphere;
+import org.jmist.toolkit.SphericalCoordinates;
+
+/**
+ * @author bkimmel
+ *
+ */
+public final class EqualSolidAnglesCollectorSphere extends
+		AbstractCollectorSphere {
+
+	public EqualSolidAnglesCollectorSphere(int sensors) {
+		super(sensors);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EqualSolidAnglesCollectorSphere(EqualSolidAnglesCollectorSphere other) {
+		super(other);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jmist.framework.measurement.AbstractCollectorSphere#clone()
+	 */
+	@Override
+	public CollectorSphere clone() {
+		return new EqualSolidAnglesCollectorSphere(this);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jmist.framework.measurement.CollectorSphere#getSensorCenter(int)
+	 */
+	@Override
+	public SphericalCoordinates getSensorCenter(int sensor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jmist.framework.measurement.CollectorSphere#getSensorProjectedSolidAngle(int)
+	 */
+	@Override
+	public double getSensorProjectedSolidAngle(int sensor) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jmist.framework.measurement.CollectorSphere#getSensorSolidAngle(int)
+	 */
+	@Override
+	public double getSensorSolidAngle(int sensor) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
