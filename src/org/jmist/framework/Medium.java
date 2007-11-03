42dcbcb2be71a216d2626c05dc5f0ba3cea4d960
Brad Kimmel <intrepidca@gmail.com>
2007-11-03 00:44:06 +0000

    Added Medium.VACCUM.

--- src/org/jmist/framework/Medium.java
+++ src/org/jmist/framework/Medium.java
@@ -16,4 +16,35 @@ public interface Medium {
 	Spectrum refractiveIndex(Point3 p);
 	Spectrum extinctionIndex(Point3 p);
 
+	/**
+	 * A vacuum <code>Medium</code>.
+	 */
+	public static final Medium VACUUM = new Medium() {
+
+		/* (non-Javadoc)
+		 * @see org.jmist.framework.Medium#extinctionIndex(org.jmist.toolkit.Point3)
+		 */
+		@Override
+		public Spectrum extinctionIndex(Point3 p) {
+			return Spectrum.ZERO;
+		}
+
+		/* (non-Javadoc)
+		 * @see org.jmist.framework.Medium#refractiveIndex(org.jmist.toolkit.Point3)
+		 */
+		@Override
+		public Spectrum refractiveIndex(Point3 p) {
+			return Spectrum.ONE;
+		}
+
+		/* (non-Javadoc)
+		 * @see org.jmist.framework.Medium#transmittance(org.jmist.toolkit.Ray3, double)
+		 */
+		@Override
+		public Spectrum transmittance(Ray3 ray, double distance) {
+			return Spectrum.ONE;
+		}
+
+	};
+
 }
