81234ce444695f058699cf0f76dd46ddf4b34e3a
Brad Kimmel <intrepidca@gmail.com>
2007-11-03 00:45:29 +0000

    Implemented Photometer.

--- src/org/jmist/framework/measurement/Photometer.java
+++ src/org/jmist/framework/measurement/Photometer.java
@@ -4,8 +4,16 @@
 package org.jmist.framework.measurement;
 
 import org.jmist.framework.Material;
+import org.jmist.framework.Medium;
+import org.jmist.framework.ScatterRecord;
+import org.jmist.framework.SurfacePoint;
+import org.jmist.framework.reporting.DummyProgressMonitor;
 import org.jmist.framework.reporting.ProgressMonitor;
+import org.jmist.toolkit.Point2;
+import org.jmist.toolkit.Point3;
 import org.jmist.toolkit.SphericalCoordinates;
+import org.jmist.toolkit.Tuple;
+import org.jmist.toolkit.Vector3;
 
 /**
  * @author bkimmel
@@ -22,33 +30,28 @@ public final class Photometer {
 	}
 
 	public void setSpecimen(Material specimen) {
-		// TODO implement this method.
-
+		this.specimen = specimen;
 	}
 
 	public void setIncidentAngle(SphericalCoordinates incident) {
-		// TODO implement this method.
-
+		this.incident = incident;
+		this.in = incident.unit().opposite().toCartesian();
 	}
 
-	public void setWavelength(double lambda) {
-		// TODO implement this method.
-
+	public void setWavelength(double wavelength) {
+		this.wavelengths = new Tuple(wavelength);
 	}
 
 	public Material getSpecimen() {
-		// TODO implement this method.
-		return null;
+		return this.specimen;
 	}
 
 	public SphericalCoordinates getIncidentAngle() {
-		// TODO implement this method.
-		return null;
+		return this.incident;
 	}
 
 	public double getWavelength() {
-		// TODO implement this method.
-		return 0.0;
+		return this.wavelengths.at(0);
 	}
 
 	public CollectorSphere getCollectorSphere() {
@@ -60,8 +63,7 @@ public final class Photometer {
 	}
 
 	public void castPhotons(long n) {
-		// TODO implement this method.
-
+		this.castPhotons(n, DummyProgressMonitor.getInstance());
 	}
 
 	public void castPhotons(long n, ProgressMonitor monitor) {
@@ -69,11 +71,87 @@ public final class Photometer {
 	}
 
 	public void castPhotons(long n, ProgressMonitor monitor, long progressInterval) {
-		// TODO implement this method.
+		
+		long untilCallback = progressInterval;
+		
+		if (!monitor.notifyProgress(0.0)) {
+			monitor.notifyCancelled();
+			return;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			
+			if (--untilCallback <= 0) {
+				
+				double progress = (double) i / (double) n;
+				
+				if (!monitor.notifyProgress(progress)) {
+					monitor.notifyCancelled();
+					return;
+				}
+				
+				untilCallback = progressInterval;
+				
+			}
+			
+			ScatterRecord rec = this.specimen.scatter(this.x, this.in, this.wavelengths);
+			
+			if (random.nextDouble() < rec.weightAt(0)) {
+				this.collectorSphere.record(rec.scatteredRay().direction());
+			}
+			
+		}
 
+		monitor.notifyComplete();
+		
 	}
 
 	private final CollectorSphere collectorSphere;
+	private Material specimen;
+	private SphericalCoordinates incident;
+	private Vector3 in;
+	private Tuple wavelengths;
+	
+	private final SurfacePoint x = new SurfacePoint() {
+
+		@Override
+		public Point3 location() {
+			return Point3.ORIGIN;
+		}
+
+		@Override
+		public Material material() {
+			return specimen;
+		}
+
+		@Override
+		public Medium ambientMedium() {
+			return Medium.VACUUM;
+		}
+
+		@Override
+		public Vector3 microfacetNormal() {
+			return Vector3.K;
+		}
+
+		@Override
+		public Vector3 normal() {
+			return Vector3.K;
+		}
+
+		@Override
+		public Vector3 tangent() {
+			return Vector3.I;
+		}
+
+		@Override
+		public Point2 textureCoordinates() {
+			return Point2.ORIGIN;
+		}
+		
+	};
+
+	private static final java.util.Random random = new java.util.Random();
 
 	private static final long DEFAULT_PROGRESS_INTERVAL = 1000;
 
