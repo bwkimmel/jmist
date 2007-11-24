8f4a140ce71f21878066e4156f59df9231235b2c
Brad Kimmel <intrepidca@gmail.com>
2007-11-24 03:51:35 +0000

    Implemented TorusGeometry.

--- src/org/jmist/packages/geometry/primitive/TorusGeometry.java
+++ src/org/jmist/packages/geometry/primitive/TorusGeometry.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.jmist.packages.geometry.primitive;
 
@@ -7,21 +7,32 @@ import org.jmist.framework.IntersectionRecorder;
 import org.jmist.framework.Material;
 import org.jmist.framework.SingleMaterialGeometry;
 import org.jmist.toolkit.Box3;
+import org.jmist.toolkit.Point2;
+import org.jmist.toolkit.Point3;
+import org.jmist.toolkit.Polynomial;
 import org.jmist.toolkit.Ray3;
 import org.jmist.toolkit.Sphere;
+import org.jmist.toolkit.Vector3;
 
 /**
+ * A torus primitive <code>Geometry</code>.
  * @author bkimmel
- *
  */
 public final class TorusGeometry extends SingleMaterialGeometry {
 
 	/**
-	 * @param material
+	 * Creates a new <code>TorusGeometry</code>.
+	 * @param major The major radius of the torus (i.e., the distance from the
+	 * 		center of the torus to a point in the center of the tube.
+	 * @param minor The minor radius of the torus (i.e., the radius of the
+	 * 		tube).
+	 * @param material The <code>Material</code> to apply to this
+	 * 		<code>TorusGeometry</code>.
 	 */
-	public TorusGeometry(Material material) {
+	public TorusGeometry(double major, double minor, Material material) {
 		super(material);
-		// TODO Auto-generated constructor stub
+		this.major = major;
+		this.minor = minor;
 	}
 
 	/* (non-Javadoc)
@@ -29,7 +40,87 @@ public final class TorusGeometry extends SingleMaterialGeometry {
 	 */
 	@Override
 	public void intersect(Ray3 ray, IntersectionRecorder recorder) {
-		// TODO Auto-generated method stub
+
+		Vector3		orig			= ray.origin().vectorFrom(Point3.ORIGIN);
+		Vector3		dir				= ray.direction().unit();
+		double		sqRadius1		= major * major;
+		double		sqRadius2		= minor * minor;
+		double		s2NormOfDir		= dir.squaredLength();
+		double		s2NormOfOrig	= orig.squaredLength();
+		double		dirDotOrig		= dir.dot(orig);
+		double		K				= s2NormOfOrig - (sqRadius1 + sqRadius2);
+
+		Polynomial	f = new Polynomial(
+							K * K - 4.0 * sqRadius1 * (sqRadius2 - orig.y() * orig.y()),
+							4.0 * dirDotOrig * (s2NormOfOrig - (sqRadius1 + sqRadius2)) + 8.0 * sqRadius1 * dir.y() * orig.y(),
+							2.0 * s2NormOfDir * (s2NormOfOrig - (sqRadius1 + sqRadius2)) + 4.0 * ((dirDotOrig * dirDotOrig) + sqRadius1 * dir.y() * dir.y()),
+							4.0 * dirDotOrig * s2NormOfDir,
+							s2NormOfDir * s2NormOfDir
+					);
+
+		double[]	x = f.roots();
+
+		if (x.length > 1)
+		{
+
+			// sort roots
+			for (int i = 1; i < x.length; i++)
+			{
+				if (x[i] < x[i - 1])
+				{
+					double	temp = x[i];
+					int		j;
+
+					for (j = i - 2; j >= 0; j--)
+						if (x[i] > x[j]) break;
+
+					x[i] = x[++j];
+					x[j] = temp;
+				}
+			}
+
+			for (int i = 0; i < x.length; i++)
+				super.newIntersection(ray, x[i], i % 2 == 0);
+
+		}
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jmist.framework.AbstractGeometry#getNormal(org.jmist.framework.AbstractGeometry.GeometryIntersection)
+	 */
+	@Override
+	protected Vector3 getNormal(GeometryIntersection x) {
+
+		Point3	p = x.location();
+		Vector3	rel = new Vector3(p.x(), 0.0, p.z());
+
+		double	length = rel.length();
+
+		if (length > 0.0)
+		{
+			rel = rel.times(major / length);
+			return p.vectorFrom(Point3.ORIGIN.plus(rel));
+		}
+		else
+			return Vector3.K;
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jmist.framework.AbstractGeometry#getTextureCoordinates(org.jmist.framework.AbstractGeometry.GeometryIntersection)
+	 */
+	@Override
+	protected Point2 getTextureCoordinates(GeometryIntersection x) {
+
+		Vector3	cp	= x.location().vectorFrom(Point3.ORIGIN);
+		Vector3	R	= new Vector3(cp.x(), 0.0, cp.z()).unit();
+		Vector3	r	= cp.minus(R.times(major)).unit();
+
+		return new Point2(
+			(Math.PI + Math.atan2(-cp.z(), cp.x())) / (2.0 * Math.PI),
+			(Math.PI + Math.atan2(cp.y(), R.dot(r))) / (2.0 * Math.PI)
+		);
 
 	}
 
@@ -38,8 +129,7 @@ public final class TorusGeometry extends SingleMaterialGeometry {
 	 */
 	@Override
 	public boolean isClosed() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	/* (non-Javadoc)
@@ -47,8 +137,10 @@ public final class TorusGeometry extends SingleMaterialGeometry {
 	 */
 	@Override
 	public Box3 boundingBox() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Box3(
+				-(major + minor), -minor, -(major + minor),
+				  major + minor ,  minor,   major + minor
+		);
 	}
 
 	/* (non-Javadoc)
@@ -56,8 +148,18 @@ public final class TorusGeometry extends SingleMaterialGeometry {
 	 */
 	@Override
 	public Sphere boundingSphere() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Sphere(Point3.ORIGIN, major + minor);
 	}
+
+	/**
+	 * The major radius of the torus (i.e., the distance from the center of the
+	 * torus to a point in the center of the tube.
+	 */
+	private final double major;
+
+	/**
+	 * The minor radius of the torus (i.e., the radius of the tube).
+	 */
+	private final double minor;
 
 }
