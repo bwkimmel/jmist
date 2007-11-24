35364621321c61becfbc0045606976194d8bad83
Brad Kimmel <intrepidca@gmail.com>
2007-11-24 04:49:38 +0000

    Extended VisibilityRayShader to allow the values to assign for a hit and for a miss to be specified in the constructor.

--- src/org/jmist/packages/VisibilityRayShader.java
+++ src/org/jmist/packages/VisibilityRayShader.java
@@ -22,6 +22,22 @@ public final class VisibilityRayShader implements RayShader {
 	 */
 	public VisibilityRayShader(VisibilityFunction3 visibilityFunction) {
 		this.visibilityFunction = visibilityFunction;
+		this.hitValue = 0.0;
+		this.missValue = 1.0;
+	}
+
+	/**
+	 * Initializes the visibility function to evaluate and the values to assign
+	 * to rays that hit or do not hit an object.
+	 * @param visibilityFunction The visibility function to evaluate.
+	 * @param hitValue The value to assign to rays that hit an object.
+	 * @param missValue The value to assign to rays that do not hit an object.
+	 */
+	public VisibilityRayShader(VisibilityFunction3 visibilityFunction,
+			double hitValue, double missValue) {
+		this.visibilityFunction = visibilityFunction;
+		this.hitValue = 0.0;
+		this.missValue = 1.0;
 	}
 
 	/* (non-Javadoc)
@@ -30,14 +46,20 @@ public final class VisibilityRayShader implements RayShader {
 	public double[] shadeRay(Ray3 ray, double[] pixel) {
 		pixel = ArrayUtil.initialize(pixel, 1);
 		if (this.visibilityFunction.visibility(ray, Interval.POSITIVE)) {
-			pixel[0] = 1.0;
+			pixel[0] = this.missValue;
 		} else {
-			pixel[0] = 0.0;
+			pixel[0] = this.hitValue;
 		}
 		return pixel;
 	}
 
 	/** The visibility function to evaluate. */
 	private final VisibilityFunction3 visibilityFunction;
+
+	/** The value to assign to rays that hit an object. */
+	private final double hitValue;
+
+	/** The value to assign to rays that do not hit an object. */
+	private final double missValue;
 
 }
