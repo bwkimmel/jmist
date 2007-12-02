75880414f407cf80213f693c1f5fe080d90e61a4
Brad Kimmel <intrepidca@gmail.com>
2007-12-02 03:48:51 +0000

    Added checks to ensure that rows and columns are non-negative.

--- src/org/jmist/toolkit/Matrix.java
+++ src/org/jmist/toolkit/Matrix.java
@@ -15,8 +15,8 @@ public final class Matrix {
 	 * 		elements of the matrix (must be large enough so that the index of
 	 * 		the last element [<code>offset + (rows-1) * rowStride + (cols-1) *
 	 * 		colStride</code>] is in the array).
-	 * @param rows The number of rows in the matrix.
-	 * @param cols The number of columns in the matrix.
+	 * @param rows The number of rows in the matrix (must be non-negative).
+	 * @param cols The number of columns in the matrix (must be non-negative).
 	 * @param offset The offset into <code>elements</code> of the first element
 	 * 		of the matrix (must be non-negative).
 	 * @param rowStride The difference between the indices into
@@ -26,6 +26,8 @@ public final class Matrix {
 	 * 		<code>elements</code> of the first element of the first column and
 	 * 		the first element of the second column.
 	 * @throws IllegalArgumentException if <code>offset</code> is negative.
+	 * @throws IllegalArgumentException if <code>rows</code> or
+	 * 		<code>cols</code> is negative.
 	 * @throws IllegalArgumentException if <code>elements</code> is not large
 	 * 		enough to hold all elements of the array).
 	 */
@@ -33,6 +35,10 @@ public final class Matrix {
 
 		if (offset < 0) {
 			throw new IllegalArgumentException("offset must be non-negative");
+		}
+
+		if (rows < 0 || cols < 0) {
+			throw new IllegalArgumentException("rows and cols must be non-negative");
 		}
 
 		if ((offset + (cols - 1) * colStride + (rows - 1) * rowStride) >= elements.length) {
@@ -138,10 +144,16 @@ public final class Matrix {
 
 	/**
 	 * Gets the element at the specified position in this <code>Matrix</code>.
-	 * @param row The row of the element to obtain.
-	 * @param col The column of the element to obtain.
+	 * @param row The row of the element to obtain (must be non-negative and
+	 * 		less than <code>this.rows()</code>).
+	 * @param col The column of the element to obtain (must be non-negative and
+	 * 		less than <code>this.columns()</code>).
 	 * @return The element at the specified row and column in this
 	 * 		<code>Matrix</code>.
+	 * @throws IllegalArgumentException if <code>row</code> or <code>col</code>
+	 * 		is out of bounds.
+	 * @see #rows()
+	 * @see #columns()
 	 */
 	public double at(int row, int col) {
 		return this.elements[indexOf(row, col)];
@@ -149,10 +161,16 @@ public final class Matrix {
 
 	/**
 	 * Gets the index into {@link #elements} of the specified row and column.
-	 * @param row The row of the element to get the index of.
-	 * @param col The column of the element to get the index of.
+	 * @param row The row of the element to get the index of (must be
+	 * 		non-negative and less than <code>this.rows()</code>).
+	 * @param col The column of the element to get the index of (must be
+	 * 		non-negative and less than <code>this.columns()</code>).
 	 * @return The index of the element at the specified row and column in this
 	 * 		<code>Matrix</code>.
+	 * @throws IllegalArgumentException if <code>row</code> or <code>col</code>
+	 * 		is out of bounds.
+	 * @see #rows()
+	 * @see #columns()
 	 */
 	private int indexOf(int row, int col) {
 		if (0 <= row && row < rows && 0 <= col && col < cols) {
@@ -179,16 +197,28 @@ public final class Matrix {
 
 	/**
 	 * Gets a view of a sub-matrix of this <code>Matrix</code> (i.e.,
-	 * @param row The index of the first row of the sub-matrix.
-	 * @param col The index of the first column of the sub-matrix.
-	 * @param rows The number of rows of the sub-matrix.
-	 * @param cols The number of columns of the sub-matrix.
+	 * @param row The index of the first row of the sub-matrix (must be
+	 * 		non-negative).
+	 * @param col The index of the first column of the sub-matrix (must be
+	 * 		non-negative).
+	 * @param rows The number of rows of the sub-matrix (must be non-negative
+	 * 		and satisfy <code>row + rows &lt;= this.rows()</code>).
+	 * @param cols The number of columns of the sub-matrix (must be
+	 * 		non-negative and satisfy
+	 * 		<code>col + cols &lt;= this.columns()</code>).
 	 * @return The <code>Matrix</code>, <code>T</code>, with <code>rows</code>
 	 * 		rows and <code>cols</code> columns satisfying
 	 * 		<code>T.at(i, j) == this.at(row + i, col + j)</code> for all
 	 * 		<code>i, j</code> with <code>0 &lt;= i &lt; rows</code> and
 	 * 		<code>0 &lt;= j &lt; cols</code>.
+	 * @throws IllegalArgumentException if <code>row</code> is negative,
+	 * 		<code>col</code> is negative, <code>rows</code> is negative,
+	 * 		<code>cols</code> is negative,
+	 * 		<code>row + rows &gt; this.rows()</code>, or
+	 * 		<code>col + cols &gt; this.columns()</code>.
 	 * @see #at(int, int)
+	 * @see #rows()
+	 * @see #columns()
 	 */
 	public Matrix slice(int row, int col, int rows, int cols) {
 		return new Matrix(elements, rows, cols, indexOf(row, col), rowStride, colStride);
@@ -209,8 +239,12 @@ public final class Matrix {
 
 	/**
 	 * Gets the specified row of this <code>Matrix</code>.
-	 * @param row The row to get.
+	 * @param row The row to get (must be non-negative and less than
+	 * 		<code>this.rows()</code>).
 	 * @return The specified row of this <code>Matrix</code>.
+	 * @throws IllegalArgumentException if <code>row</code> is negative or if
+	 * 		<code>row &gt;= this.rows()</code>.
+	 * @see #rows()
 	 */
 	public Matrix row(int row) {
 		return slice(row, 0, 1, cols);
@@ -218,8 +252,12 @@ public final class Matrix {
 
 	/**
 	 * Gets the specified column of this <code>Matrix</code>.
-	 * @param col The column to get.
+	 * @param col The column to get (must be non-negative and less than
+	 * 		<code>this.columns()</code>).
 	 * @return The specified column of this <code>Matrix</code>.
+	 * @throws IllegalArgumentException if <code>col</code> is negative or if
+	 * 		<code>col &gt;= this.columns()</code>.
+	 * @see #columns()
 	 */
 	public Matrix column(int col) {
 		return slice(0, col, rows, 1);
@@ -227,9 +265,13 @@ public final class Matrix {
 
 	/**
 	 * Gets a <code>Matrix</code> consisting of all zeros.
-	 * @param rows The number of rows in the <code>Matrix</code>.
-	 * @param cols The number of columns in the <code>Matrix</code>.
+	 * @param rows The number of rows in the <code>Matrix</code> (must be
+	 * 		non-negative).
+	 * @param cols The number of columns in the <code>Matrix</code> (must be
+	 * 		non-negative).
 	 * @return The zero <code>Matrix</code> with the specified dimensions.
+	 * @throws IllegalArgumentException if <code>rows</code> or
+	 * 		<code>cols</code> is negative.
 	 */
 	public static Matrix zeros(int rows, int cols) {
 		return new Matrix(ZERO, rows, cols, 0, 0, 0);
@@ -237,10 +279,14 @@ public final class Matrix {
 
 	/**
 	 * Gets a <code>Matrix</code> consisting of all ones.
-	 * @param rows The number of rows in the <code>Matrix</code>.
-	 * @param cols The number of columns in the <code>Matrix</code>.
+	 * @param rows The number of rows in the <code>Matrix</code> (must be
+	 * 		non-negative).
+	 * @param cols The number of columns in the <code>Matrix</code> (must be
+	 * 		non-negative).
 	 * @return The <code>Matrix</code> with the specified dimensions consisting
 	 * 		of all ones.
+	 * @throws IllegalArgumentException if <code>rows</code> or
+	 * 		<code>cols</code> is negative.
 	 */
 	public static Matrix ones(int rows, int cols) {
 		return new Matrix(ONE, rows, cols, 0, 0, 0);
@@ -248,9 +294,13 @@ public final class Matrix {
 
 	/**
 	 * Gets a <code>Matrix</code> with each element having the same value.
-	 * @param rows The number of rows in the <code>Matrix</code>.
-	 * @param cols The number of columns in the <code>Matrix</code>.
+	 * @param rows The number of rows in the <code>Matrix</code> (must be
+	 * 		non-negative).
+	 * @param cols The number of columns in the <code>Matrix</code> (must be
+	 * 		non-negative).
 	 * @return The <code>Matrix</code> with all elements having the same value.
+	 * @throws IllegalArgumentException if <code>rows</code> or
+	 * 		<code>cols</code> is negative.
 	 */
 	public static Matrix constant(int rows, int cols, double value) {
 		return new Matrix(new double[]{ value }, rows, cols, 0, 0, 0);
@@ -258,9 +308,10 @@ public final class Matrix {
 
 	/**
 	 * Gets the identity <code>Matrix</code> of the specified size.
-	 * @param n The size of the <code>Matrix</code>.
+	 * @param n The size of the <code>Matrix</code> (must be non-negative).
 	 * @return The <code>n</code> by <code>n</code> identity
 	 * 		<code>Matrix</code>.
+	 * @throws IllegalArgumentException if <code>n</code> is negative.
 	 */
 	public static Matrix identity(int n) {
 		int size = n * n;
@@ -274,8 +325,10 @@ public final class Matrix {
 	/**
 	 * Creates a <code>Matrix</code> with the specified values in column-major
 	 * order.
-	 * @param rows The number of rows in the <code>Matrix</code>.
-	 * @param cols The number of columns in the <code>Matrix</code>.
+	 * @param rows The number of rows in the <code>Matrix</code> (must be
+	 * 		non-negative).
+	 * @param cols The number of columns in the <code>Matrix</code> (must be
+	 * 		non-negative).
 	 * @param elements The elements of the <code>Matrix</code> in column-major
 	 * 		order (must have at least <code>rows * cols</code> elements --
 	 * 		additional elements will be ignored).
@@ -283,6 +336,8 @@ public final class Matrix {
 	 * 		consisting of the elements specified.
 	 * @throws IllegalArgumentException if
 	 * 		<code>elements.length &lt; rows * cols</code>.
+	 * @throws IllegalArgumentException if <code>rows</code> or
+	 * 		<code>cols</code> is negative.
 	 */
 	public static Matrix columnMajor(int rows, int cols, double[] elements) {
 		return new Matrix(elements, rows, cols, 0, 1, rows);
@@ -291,8 +346,10 @@ public final class Matrix {
 	/**
 	 * Creates a <code>Matrix</code> with the specified values in row-major
 	 * order.
-	 * @param rows The number of rows in the <code>Matrix</code>.
-	 * @param cols The number of columns in the <code>Matrix</code>.
+	 * @param rows The number of rows in the <code>Matrix</code> (must be
+	 * 		non-negative).
+	 * @param cols The number of columns in the <code>Matrix</code> (must be
+	 * 		non-negative).
 	 * @param elements The elements of the <code>Matrix</code> in row-major
 	 * 		order (must have at least <code>rows * cols</code> elements --
 	 * 		additional elements will be ignored).
@@ -300,6 +357,8 @@ public final class Matrix {
 	 * 		consisting of the elements specified.
 	 * @throws IllegalArgumentException if
 	 * 		<code>elements.length &lt; rows * cols</code>.
+	 * @throws IllegalArgumentException if <code>rows</code> or
+	 * 		<code>cols</code> is negative.
 	 */
 	public static Matrix rowMajor(int rows, int cols, double[] elements) {
 		return new Matrix(elements, rows, cols, 0, cols, 1);
