dfb7a339e8dacb9444ee90ad291415a8e88ec3dd
Brad Kimmel <intrepidca@gmail.com>
2007-12-13 02:13:28 +0000

    Added CategoricalRandom.

---
+++ src/org/jmist/framework/CategoricalRandom.java
@@ -0,0 +1,68 @@
+/**
+ *
+ */
+package org.jmist.framework;
+
+import java.util.Arrays;
+
+import org.jmist.packages.SimpleRandom;
+
+/**
+ * A categorical random variable (i.e., a discrete random variable that selects
+ * a value with probability proportional to specified weights).
+ * @author bkimmel
+ */
+public final class CategoricalRandom {
+
+	/**
+	 * Creates a new <code>CategoricalRandom</code>.
+	 * @param weights An array of the weights associated with each integer
+	 * 		from zero to <code>weights.length - 1</code>.
+	 * @param source The <code>Random</code> number generator to use to seed
+	 * 		this <code>CategoricalRandom</code>.
+	 */
+	public CategoricalRandom(double[] weights, Random source) {
+
+		this.source = source;
+		this.cpf = weights.clone();
+
+		/* Compute the cumulative probability function. */
+		for (int i = 1; i < cpf.length; i++) {
+			this.cpf[i] += this.cpf[i - 1];
+		}
+
+		for (int i = 0; i < cpf.length; i++) {
+			this.cpf[i] /= this.cpf[cpf.length - 1];
+		}
+
+	}
+
+	/**
+	 * Creates a new <code>CategoricalRandom</code>.
+	 * @param weights An array of the weights associated with each integer
+	 * 		from zero to <code>weights.length - 1</code>.
+	 */
+	public CategoricalRandom(double[] weights) {
+		this(weights, new SimpleRandom());
+	}
+
+	/**
+	 * Generates a new sample of this <code>CategoricalRandom</code>
+	 * variable.
+	 * @return The next sample.
+	 */
+	public int next() {
+		int index = Arrays.binarySearch(this.cpf, source.next());
+		return index >= 0 ? index : -(index + 1);
+	}
+
+	/** The cumulative probability function. */
+	private final double[] cpf;
+
+	/**
+	 * The <code>Random</code> number generator to use to seed this
+	 * <code>CategoricalRandom</code>.
+	 */
+	private final Random source;
+
+}
