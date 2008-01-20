d66d1f8ee403573aec8ad344ab633d40a589ea36
Brad Kimmel <intrepidca@gmail.com>
2008-01-20 17:31:48 +0000

    Added org.jmist.framework.services.WorkerClient.

---
+++ src/org/jmist/framework/services/WorkerClient.java
@@ -0,0 +1,40 @@
+/**
+ *
+ */
+package org.jmist.framework.services;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import javax.swing.JDialog;
+
+import org.jmist.framework.Job;
+import org.jmist.framework.reporting.ProgressTreePanel;
+
+/**
+ * @author brad
+ *
+ */
+public final class WorkerClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		String host = "spock.eandb.net";
+		JDialog dialog = new JDialog();
+		dialog.setTitle(host);
+		ProgressTreePanel monitor = new ProgressTreePanel();
+		Executor threadPool = Executors.newFixedThreadPool(2, new BackgroundThreadFactory());
+		Job workerJob = new ThreadServiceWorkerJob(host, 10000, 2, threadPool);
+
+		dialog.add(monitor);
+		dialog.setBounds(0, 0, 400, 300);
+		dialog.setVisible(true);
+
+		workerJob.go(monitor);
+
+	}
+
+}
