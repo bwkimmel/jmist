d3004cc088d032cb1450c63eb24d6926195383fb
Brad Kimmel <intrepidca@gmail.com>
2008-03-31 05:09:27 +0000

    Modified WorkerClient so that it shuts down when the user closes the window.

--- src/org/jmist/framework/services/WorkerClient.java
+++ src/org/jmist/framework/services/WorkerClient.java
@@ -3,6 +3,8 @@
  */
 package org.jmist.framework.services;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.rmi.RMISecurityManager;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -28,14 +30,27 @@ public final class WorkerClient {
         }
 
 		String host = args.length > 0 ? args[0] : "localhost";
-		JDialog dialog = new JDialog();
-		ProgressPanel monitor = new ProgressPanel();
 
 		int numberOfCpus = Runtime.getRuntime().availableProcessors();
 		Executor threadPool = Executors.newFixedThreadPool(numberOfCpus, new BackgroundThreadFactory());
 		Job workerJob = new ThreadServiceWorkerJob(host, 10000, numberOfCpus, threadPool);
 
+		ProgressPanel monitor = new ProgressPanel();
 		monitor.setRootVisible(false);
+
+		final JDialog dialog = new JDialog();
+		dialog.addWindowListener(new WindowAdapter() {
+
+			/* (non-Javadoc)
+			 * @see java.awt.event.WindowAdapter#windowClosing(java.awt.event.WindowEvent)
+			 */
+			@Override
+			public void windowClosing(WindowEvent e) {
+				dialog.setVisible(false);
+				System.exit(0);
+			}
+
+		});
 
 		dialog.add(monitor);
 		dialog.setBounds(0, 0, 400, 300);
