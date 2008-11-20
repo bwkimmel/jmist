9ff492bb71d91ec36b0f037b20c60757c7b68520
Brad Kimmel <intrepidca@gmail.com>
2008-11-20 03:17:03 +0000

    Added class management to JobServer.

--- src/org/jdcp/server/JobServer.java
+++ src/org/jdcp/server/JobServer.java
@@ -3,20 +3,28 @@
  */
 package org.jdcp.server;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.nio.ByteBuffer;
 import java.rmi.RemoteException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import org.jdcp.job.ParallelizableJob;
 import org.jdcp.job.TaskDescription;
 import org.jdcp.job.TaskWorker;
 import org.jdcp.remote.JobService;
 import org.jdcp.scheduling.TaskScheduler;
+import org.jdcp.server.classmanager.ClassManager;
+import org.jdcp.server.classmanager.ParentClassManager;
 import org.selfip.bkimmel.progress.ProgressMonitor;
 import org.selfip.bkimmel.rmi.Envelope;
-import org.selfip.bkimmel.util.classloader.MapClassLoaderStrategy;
 import org.selfip.bkimmel.util.classloader.StrategyClassLoader;
 
 /**
@@ -29,17 +37,29 @@ public final class JobServer implements JobService {
 
 	private final TaskScheduler scheduler;
 
+	private final ParentClassManager classManager;
+
+	private final File outputDirectory;
+
 	private final Map<UUID, ScheduledJob> jobs = new HashMap<UUID, ScheduledJob>();
 
 	/**
 	 * Creates a new <code>JobServer</code>.
+	 * @param outputDirectory The directory to write job results to.
 	 * @param monitor The <code>ProgressMonitor</code> to report to.
 	 * @param scheduler The <code>TaskScheduler</code> to use to assign
 	 * 		tasks.
+	 * @param classManager The <code>ParentClassManager</code> to use to
+	 * 		store and retrieve class definitions.
 	 */
-	public JobServer(ProgressMonitor monitor, TaskScheduler scheduler) {
+	public JobServer(File outputDirectory, ProgressMonitor monitor, TaskScheduler scheduler, ParentClassManager classManager) throws IllegalArgumentException {
+		if (!outputDirectory.isDirectory()) {
+			throw new IllegalArgumentException("outputDirectory must be a directory.");
+		}
+		this.outputDirectory = outputDirectory;
 		this.monitor = monitor;
 		this.scheduler = scheduler;
+		this.classManager = classManager;
 	}
 
 	/* (non-Javadoc)
@@ -83,14 +103,14 @@ public final class JobServer implements JobService {
 	 * @see org.jdcp.remote.JobService#cancelJob(java.util.UUID)
 	 */
 	public void cancelJob(UUID jobId) throws IllegalArgumentException, SecurityException, RemoteException {
-
-		if (!jobs.containsKey(jobId)) {
+		ScheduledJob sched = jobs.get(jobId);
+		if (sched == null) {
 			throw new IllegalArgumentException("No job with provided Job ID");
 		}
 
 		jobs.remove(jobId);
 		scheduler.removeJob(jobId);
-
+		classManager.releaseChildClassManager(sched.classManager);
 	}
 
 	/* (non-Javadoc)
@@ -127,7 +147,11 @@ public final class JobServer implements JobService {
 			throw new IllegalArgumentException("No submitted job with provided Job ID");
 		}
 
-		sched.submitTaskResults(taskId, results);
+		if (sched.submitTaskResults(taskId, results)) { // job is complete
+			jobs.remove(jobId);
+			scheduler.removeJob(jobId);
+			classManager.releaseChildClassManager(sched.classManager);
+		}
 	}
 
 	/* (non-Javadoc)
@@ -135,8 +159,19 @@ public final class JobServer implements JobService {
 	 */
 	public byte[] getClassDefinition(String name, UUID jobId)
 			throws SecurityException, RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		ScheduledJob sched = jobs.get(jobId);
+		if (sched == null) {
+			throw new IllegalArgumentException("No job with provided Job ID");
+		}
+
+		ByteBuffer def = sched.classManager.getClassDefinition(name);
+		if (def.hasArray() && def.arrayOffset() == 0) {
+			return def.array();
+		} else {
+			byte[] bytes = new byte[def.remaining()];
+			def.get(bytes);
+			return bytes;
+		}
 	}
 
 	/* (non-Javadoc)
@@ -144,8 +179,12 @@ public final class JobServer implements JobService {
 	 */
 	public byte[] getClassDigest(String name, UUID jobId)
 			throws SecurityException, RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		ScheduledJob sched = jobs.get(jobId);
+		if (sched == null) {
+			throw new IllegalArgumentException("No job with provided Job ID");
+		}
+
+		return sched.classManager.getClassDigest(name);
 	}
 
 	/* (non-Javadoc)
@@ -153,8 +192,7 @@ public final class JobServer implements JobService {
 	 */
 	public byte[] getClassDigest(String name) throws SecurityException,
 			RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		return classManager.getClassDigest(name);
 	}
 
 	/* (non-Javadoc)
@@ -162,8 +200,7 @@ public final class JobServer implements JobService {
 	 */
 	public void setClassDefinition(String name, byte[] def)
 			throws SecurityException, RemoteException {
-		// TODO Auto-generated method stub
-
+		classManager.setClassDefinition(name, def);
 	}
 
 	/* (non-Javadoc)
@@ -171,8 +208,12 @@ public final class JobServer implements JobService {
 	 */
 	public void setClassDefinition(String name, UUID jobId, byte[] def)
 			throws IllegalArgumentException, SecurityException, RemoteException {
-		// TODO Auto-generated method stub
+		ScheduledJob sched = jobs.get(jobId);
+		if (sched == null || sched.job != null) {
+			throw new IllegalArgumentException("No pending job with provided Job ID");
+		}
 
+		sched.classManager.setClassDefinition(name, def);
 	}
 
 	/* (non-Javadoc)
@@ -221,7 +262,11 @@ public final class JobServer implements JobService {
 		 */
 		public final ProgressMonitor			monitor;
 
-		private final Map<String, ByteBuffer>	classDefs = new HashMap<String, ByteBuffer>();
+		/**
+		 * The <code>ClassManager</code> to use to store the class definitions
+		 * applicable to this job.
+		 */
+		public final ClassManager				classManager;
 
 		/**
 		 * Initializes the scheduled job.
@@ -239,6 +284,8 @@ public final class JobServer implements JobService {
 
 			//String title		= String.format("%s (%s)", this.job.getClass().getSimpleName(), this.id.toString());
 			this.monitor		= monitor.createChildProgressMonitor(description);
+			this.classManager	= JobServer.this.classManager.createChildClassManager();
+
 			this.initializeJob(job);
 
 		}
@@ -259,23 +306,66 @@ public final class JobServer implements JobService {
 			this.monitor		= monitor.createChildProgressMonitor(description);
 			this.monitor.notifyStatusChanged("Awaiting job submission");
 
+			this.classManager	= JobServer.this.classManager.createChildClassManager();
+
 		}
 
 		public void initializeJob(Envelope<ParallelizableJob> job) throws ClassNotFoundException {
-			ClassLoader loader	= new StrategyClassLoader(new MapClassLoaderStrategy(classDefs));
+			ClassLoader loader	= new StrategyClassLoader(classManager);
 			this.job			= job.contents(loader);
 			this.worker			= new Envelope<TaskWorker>(this.job.worker());
 			this.monitor.notifyStatusChanged("");
 		}
 
-		public void submitTaskResults(int taskId, Envelope<Object> results) throws ClassNotFoundException {
+		public boolean submitTaskResults(int taskId, Envelope<Object> results) throws ClassNotFoundException {
 			ClassLoader cl = job.getClass().getClassLoader();
 			Object task = scheduler.remove(id, taskId);
 			job.submitTaskResults(task, results.contents(cl), monitor);
+
+			if (job.isComplete()) {
+				writeJobResults();
+				return true;
+			}
+
+			return false;
 		}
 
 		public void scheduleNextTask() {
 			scheduler.add(id, job.getNextTask());
+		}
+
+		/**
+		 * Writes the results of a <code>ScheduledJob</code> to the output
+		 * directory.
+		 * @param sched The <code>ScheduledJob</code> to write results for.
+		 */
+		private void writeJobResults() {
+
+			assert(job.isComplete());
+
+			try {
+
+				String				filename		= String.format("%s.zip", id.toString());
+				File				outputFile		= new File(outputDirectory, filename);
+				OutputStream		out				= new FileOutputStream(outputFile);
+				ZipOutputStream		zip				= new ZipOutputStream(out);
+
+				zip.putNextEntry(new ZipEntry("job.log"));
+
+				PrintStream			log				= new PrintStream(zip);
+				log.printf("%tc: Job %s completed.", new Date(), id.toString());
+				log.println();
+				log.flush();
+
+				zip.closeEntry();
+
+				job.writeJobResults(zip);
+				zip.close();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
 		}
 
 	}
