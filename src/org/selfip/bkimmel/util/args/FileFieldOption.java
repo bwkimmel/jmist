a3add27d2b52a60f594d7296a65b2d9a41aaa5d4
Brad Kimmel <intrepidca@gmail.com>
2008-11-27 18:38:24 +0000

    Renamed parameter args to argq for FileFieldOption.getOptionValue.

--- src/org/selfip/bkimmel/util/args/FileFieldOption.java
+++ src/org/selfip/bkimmel/util/args/FileFieldOption.java
@@ -34,8 +34,8 @@ public class FileFieldOption<T> extends AbstractFieldOption<T> {
 	 * @see org.selfip.bkimmel.util.args.AbstractFieldOption#getOptionValue(java.util.Queue)
 	 */
 	@Override
-	protected Object getOptionValue(Queue<String> args) {
-		String name = args.remove();
+	protected Object getOptionValue(Queue<String> argq) {
+		String name = argq.remove();
 		File file = new File(name);
 		if (mustExist && !file.isFile()) {
 			System.err.println("File does not exist: '" + name + "'");
