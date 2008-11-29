ae9d93e17252820cf17ddc465c2e246531651502
Brad Kimmel <intrepidca@gmail.com>
2008-11-29 18:57:54 +0000

    Organized imports.

--- src/org/selfip/bkimmel/io/FileUtil.java
+++ src/org/selfip/bkimmel/io/FileUtil.java
@@ -5,7 +5,6 @@ package org.selfip.bkimmel.io;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
