8955383565d7bb5dbdec3b1c92a094c70e0e1fac
Brad Kimmel <intrepidca@gmail.com>
2009-06-26 20:27:28 +0000

    Forgot to check in PrimitiveListGeometry.

---
+++ src/ca/eandb/jmist/framework/geometry/PrimitiveListGeometry.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2008 Bradley W. Kimmel
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package ca.eandb.jmist.framework.geometry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.eandb.jmist.framework.BoundingBoxBuilder3;
+import ca.eandb.jmist.framework.IntersectionRecorder;
+import ca.eandb.jmist.math.Box3;
+import ca.eandb.jmist.math.Ray3;
+import ca.eandb.jmist.math.Sphere;
+
+/**
+ * @author brad
+ *
+ */
+public final class PrimitiveListGeometry extends AbstractGeometry {
+
+	private final List<PrimitiveGeometry> primitives = new ArrayList<PrimitiveGeometry>();
+
+	public PrimitiveListGeometry addPrimitive(PrimitiveGeometry primitive) {
+		primitives.add(primitive);
+		return this;
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.eandb.jmist.framework.SceneElement#getBoundingBox(int)
+	 */
+	@Override
+	public Box3 getBoundingBox(int index) {
+		return primitives.get(index).boundingBox();
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.eandb.jmist.framework.SceneElement#getBoundingSphere(int)
+	 */
+	@Override
+	public Sphere getBoundingSphere(int index) {
+		return primitives.get(index).boundingSphere();
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.eandb.jmist.framework.SceneElement#getNumPrimitives()
+	 */
+	@Override
+	public int getNumPrimitives() {
+		return primitives.size();
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.eandb.jmist.framework.SceneElement#intersect(int, ca.eandb.jmist.math.Ray3, ca.eandb.jmist.framework.IntersectionRecorder)
+	 */
+	@Override
+	public void intersect(int index, Ray3 ray, IntersectionRecorder recorder) {
+		primitives.get(index).intersect(ray, recorder);
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.eandb.jmist.framework.Bounded3#boundingBox()
+	 */
+	@Override
+	public Box3 boundingBox() {
+		BoundingBoxBuilder3 builder = new BoundingBoxBuilder3();
+		for (PrimitiveGeometry primitive : primitives) {
+			builder.add(primitive.boundingBox());
+		}
+		return builder.getBoundingBox();
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.eandb.jmist.framework.Bounded3#boundingSphere()
+	 */
+	@Override
+	public Sphere boundingSphere() {
+		Box3 box = boundingBox();
+		return new Sphere(box.center(), 0.5 * box.diagonal());
+	}
+
+}
