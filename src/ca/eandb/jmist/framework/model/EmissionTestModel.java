9e49db73aebde933faa322dbad127304d0ac1928
Brad Kimmel <intrepidca@gmail.com>
2009-07-02 17:30:57 +0000

    Modified EmissionTestModel to implement Scene interface. Removed obsolete classes.

--- src/ca/eandb/jmist/framework/model/EmissionTestModel.java
+++ src/ca/eandb/jmist/framework/model/EmissionTestModel.java
@@ -3,55 +3,45 @@
  */
 package ca.eandb.jmist.framework.model;
 
-import ca.eandb.jmist.framework.SceneElement;
 import ca.eandb.jmist.framework.Lens;
 import ca.eandb.jmist.framework.Light;
 import ca.eandb.jmist.framework.Material;
-import ca.eandb.jmist.framework.Model;
+import ca.eandb.jmist.framework.Scene;
+import ca.eandb.jmist.framework.SceneElement;
 import ca.eandb.jmist.framework.color.ColorModel;
-import ca.eandb.jmist.framework.geometry.BoundingBoxHierarchyGeometry;
-import ca.eandb.jmist.framework.geometry.CompositeGeometry;
+import ca.eandb.jmist.framework.geometry.PrimitiveListGeometry;
 import ca.eandb.jmist.framework.geometry.primitive.BoxGeometry;
 import ca.eandb.jmist.framework.geometry.primitive.RectangleGeometry;
 import ca.eandb.jmist.framework.lens.PinholeLens;
 import ca.eandb.jmist.framework.lens.TransformableLens;
-import ca.eandb.jmist.framework.light.CompositeLight;
-import ca.eandb.jmist.framework.light.RandomCompositeLight;
-import ca.eandb.jmist.framework.light.SimpleCompositeLight;
 import ca.eandb.jmist.framework.material.LambertianMaterial;
 import ca.eandb.jmist.framework.painter.UniformPainter;
-import ca.eandb.jmist.framework.random.StratifiedRandom;
-import ca.eandb.jmist.framework.random.ThreadLocalRandom;
+import ca.eandb.jmist.framework.scene.MaterialMapSceneElement;
 import ca.eandb.jmist.math.Basis3;
 import ca.eandb.jmist.math.Box3;
 import ca.eandb.jmist.math.Point3;
+import ca.eandb.jmist.math.Sphere;
 import ca.eandb.jmist.math.Vector3;
 
 /**
  * @author Brad Kimmel
  *
  */
-public final class EmissionTestModel implements Model {
+public final class EmissionTestModel implements Scene {
 
 	/* (non-Javadoc)
 	 * @see ca.eandb.jmist.framework.Model#getGeometry()
 	 */
-	public SceneElement getGeometry() {
-		if (geometry == null) {
-			this.initialize();
-		}
-
-		return geometry;
+	public SceneElement getRoot() {
+		initialize();
+		return root;
 	}
 
 	/* (non-Javadoc)
 	 * @see ca.eandb.jmist.framework.Model#getLens()
 	 */
 	public Lens getLens() {
-		if (lens == null) {
-			this.initialize();
-		}
-
+		initialize();
 		return lens;
 	}
 
@@ -59,14 +49,11 @@ public final class EmissionTestModel implements Model {
 	 * @see ca.eandb.jmist.framework.Model#getLight()
 	 */
 	public Light getLight() {
-		if (light == null) {
-			this.initialize();
-		}
-
-		return light;
+		initialize();
+		return root.createLight();
 	}
 
-	public static Model getInstance() {
+	public static Scene getInstance() {
 		if (instance == null) {
 			instance = new EmissionTestModel();
 		}
@@ -74,45 +61,41 @@ public final class EmissionTestModel implements Model {
 		return instance;
 	}
 
-	private void initialize() {
+	private synchronized void initialize() {
 
-		RectangleGeometry lightCell = new RectangleGeometry(
-				new Point3(-200.0, 50.0, 0.0),
-				Basis3.fromW(Vector3.I),
-				100.0, 100.0,
-				false,
-				diffuseLuminaire1
-		);
+		if (root != null) {
+			return;
+		}
 
-		CompositeLight smallLights = new RandomCompositeLight(new ThreadLocalRandom(new StratifiedRandom(100)));
-
-		light = new SimpleCompositeLight()
-				.addChild(lightCell)
-				.addChild(smallLights);
-
-		geometry = new BoundingBoxHierarchyGeometry()
-				.addChild(lightCell)
-				.addChild(
-						new BoxGeometry(
-								new Box3(-200.0, -10.0, -200.0, 200.0, 0.0, 200.0),
-								diffuse50
-						)
-				);
+		PrimitiveListGeometry geometry = new PrimitiveListGeometry()
+			.addPrimitive(
+				new RectangleGeometry(
+					new Point3(-200.0, 50.0, 0.0),
+					Basis3.fromW(Vector3.I),
+					100.0, 100.0,
+					false))
+			.addPrimitive(
+				new BoxGeometry(
+					new Box3(-200.0, -10.0, -200.0, 200.0, 0.0, 200.0)));
 
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
-				lightCell = new RectangleGeometry(
+				geometry.addPrimitive(new RectangleGeometry(
 						new Point3(200.0, (double) (5 + (10 * i)), (double) (-45 + (10 * j))),
 						Basis3.fromW(Vector3.NEGATIVE_I),
 						10.0, 10.0,
-						false,
-						diffuseLuminaire1
-				);
-
-				geometry.addChild(lightCell);
-				smallLights.addChild(lightCell);
+						false));
 			}
 		}
+
+		MaterialMapSceneElement materials = new MaterialMapSceneElement(geometry)
+			.addMaterial("diffuse50", diffuse50)
+			.addMaterial("diffuseLuminaire1", diffuseLuminaire1)
+			.setMaterial(0, "diffuseLuminaire1")
+			.setMaterial(1, "diffuse50")
+			.setMaterialRange(2, 100, "diffuseLuminaire1");
+
+		root = materials;
 
 		lens = PinholeLens.fromHfovAndAspect(0.785398, 1.0);
 
@@ -121,14 +104,25 @@ public final class EmissionTestModel implements Model {
 
 	}
 
-	private static Model instance = null;
+	@Override
+	public Box3 boundingBox() {
+		initialize();
+		return root.boundingBox();
+	}
+
+	@Override
+	public Sphere boundingSphere() {
+		initialize();
+		return root.boundingSphere();
+	}
+
+	private static Scene instance = null;
 
 	private final ColorModel cm = ColorModel.getInstance();
 	private final Material diffuse50 = new LambertianMaterial(new UniformPainter(cm.getGray(0.5)));
-	private final Material diffuseLuminaire1 = new LambertianMaterial(null, new UniformPainter(cm.getWhite()));
+	private final Material diffuseLuminaire1 = new LambertianMaterial(null, new UniformPainter(cm.getGray(2e5)));
 
-	private CompositeGeometry geometry = null;
-	private CompositeLight light = null;
+	private SceneElement root = null;
 	private TransformableLens lens = null;
 
 }
