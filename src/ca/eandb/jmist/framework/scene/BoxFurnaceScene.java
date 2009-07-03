17746a9e955298e526474e7876eba753d7b3ed3e
Brad Kimmel <intrepidca@gmail.com>
2009-07-03 00:03:00 +0000

    Added to "furnace" scene elements (see http://www.cs.utah.edu/~bes/papers/scenes/paper-node2.html).

---
+++ src/ca/eandb/jmist/framework/scene/BoxFurnaceScene.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2008 Bradley W. Kimmel
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package ca.eandb.jmist.framework.scene;
+
+import ca.eandb.jmist.framework.Lens;
+import ca.eandb.jmist.framework.Light;
+import ca.eandb.jmist.framework.SceneElement;
+import ca.eandb.jmist.framework.color.Spectrum;
+import ca.eandb.jmist.framework.geometry.InsideOutGeometry;
+import ca.eandb.jmist.framework.geometry.primitive.BoxGeometry;
+import ca.eandb.jmist.framework.lens.FisheyeLens;
+import ca.eandb.jmist.framework.material.LambertianMaterial;
+import ca.eandb.jmist.math.Box3;
+
+/**
+ * @author brad
+ *
+ */
+public final class BoxFurnaceScene extends AbstractScene {
+
+	private static final Lens lens = new FisheyeLens();
+
+	private final SceneElement root;
+
+	public BoxFurnaceScene(Spectrum reflectance, Spectrum emittance) {
+		root = new MaterialSceneElement(
+				new LambertianMaterial(reflectance, emittance),
+				new InsideOutGeometry(new BoxGeometry(new Box3(-1, -1, -1, 1, 1, 1))));
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.eandb.jmist.framework.Scene#getLens()
+	 */
+	@Override
+	public Lens getLens() {
+		return lens;
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.eandb.jmist.framework.Scene#getLight()
+	 */
+	@Override
+	public Light getLight() {
+		return root.createLight();
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.eandb.jmist.framework.Scene#getRoot()
+	 */
+	@Override
+	public SceneElement getRoot() {
+		return root;
+	}
+
+}
