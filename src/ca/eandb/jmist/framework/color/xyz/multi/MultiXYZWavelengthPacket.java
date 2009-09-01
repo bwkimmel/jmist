50e73d692c4e9366b6292fada7a7b771f6e4e122
Brad Kimmel <intrepidca@gmail.com>
2009-09-01 14:28:46 +0000

    Added MultiXYZColorModel -- an XYZ color model that uses multiple channels for each component.

---
+++ src/ca/eandb/jmist/framework/color/xyz/multi/MultiXYZWavelengthPacket.java
@@ -0,0 +1,50 @@
+/**
+ *
+ */
+package ca.eandb.jmist.framework.color.xyz.multi;
+
+import ca.eandb.jmist.framework.color.WavelengthPacket;
+
+/**
+ * @author Brad
+ *
+ */
+/* package */ final class MultiXYZWavelengthPacket implements WavelengthPacket {
+
+	private final MultiXYZColorModel owner;
+
+	private final double[] wavelengths;
+
+	public MultiXYZWavelengthPacket(double[] wavelengths, MultiXYZColorModel owner) {
+		this.owner = owner;
+		this.wavelengths = wavelengths;
+	}
+
+	public double[] getLambda() {
+		return wavelengths;
+	}
+
+	public double getLambda(int channel) {
+		return wavelengths[channel];
+	}
+
+	public double getLambdaX(int channel) {
+		return wavelengths[channel + owner.getOffsetX()];
+	}
+
+	public double getLambdaY(int channel) {
+		return wavelengths[channel + owner.getOffsetY()];
+	}
+
+	public double getLambdaZ(int channel) {
+		return wavelengths[channel + owner.getOffsetZ()];
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.eandb.jmist.framework.color.WavelengthPacket#getColorModel()
+	 */
+	public MultiXYZColorModel getColorModel() {
+		return owner;
+	}
+
+}
