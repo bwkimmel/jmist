e41c776baa961a8bc0041246d49737ff01908335
Brad Kimmel <intrepidca@gmail.com>
2010-01-13 14:22:25 +0000

    Extracted geometric methods out of SurfacePoint to SurfacePointGeometry.

--- src/ca/eandb/jmist/framework/SurfacePoint.java
+++ src/ca/eandb/jmist/framework/SurfacePoint.java
@@ -3,28 +3,12 @@
  */
 package ca.eandb.jmist.framework;
 
-import ca.eandb.jmist.math.Basis3;
-import ca.eandb.jmist.math.Point2;
-import ca.eandb.jmist.math.Point3;
-import ca.eandb.jmist.math.Vector3;
 
 /**
  * @author Brad Kimmel
  *
  */
-public interface SurfacePoint {
-
-	Point3 getPosition();
-	Vector3 getNormal();
-	Basis3 getBasis();
-	Vector3 getShadingNormal();
-	Basis3 getShadingBasis();
-
-	Vector3 getTangent();
-
-	Point2 getUV();
-
-	int getPrimitiveIndex();
+public interface SurfacePoint extends SurfacePointGeometry {
 
 	Material getMaterial();
 
---
+++ src/ca/eandb/jmist/framework/SurfacePointGeometry.java
@@ -0,0 +1,26 @@
+package ca.eandb.jmist.framework;
+
+import ca.eandb.jmist.math.Basis3;
+import ca.eandb.jmist.math.Point2;
+import ca.eandb.jmist.math.Point3;
+import ca.eandb.jmist.math.Vector3;
+
+public interface SurfacePointGeometry {
+
+	Point3 getPosition();
+
+	Vector3 getNormal();
+
+	Basis3 getBasis();
+
+	Vector3 getShadingNormal();
+
+	Basis3 getShadingBasis();
+
+	Vector3 getTangent();
+
+	Point2 getUV();
+
+	int getPrimitiveIndex();
+
+}
