7a5aef57d864d3066616149c807ced0c42cb5288
Brad Kimmel <intrepidca@gmail.com>
2010-09-04 20:35:33 +0000

    Added two-dimensional transform interfaces.

---
+++ src/ca/eandb/jmist/framework/AffineTransformable2.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+package ca.eandb.jmist.framework;
+
+import ca.eandb.jmist.math.AffineMatrix2;
+
+
+/**
+ * A two dimensional object that can be transformed according to an affine transformation
+ * (linear transformations plus translations).
+ * @author Brad Kimmel
+ *
+ */
+public interface AffineTransformable2 extends LinearTransformable2, ShapePreservingTransformable2, Stretchable2 {
+
+	/**
+	 * Transforms this object according to the specified affine transformation.
+	 * @param T The matrix representing the affine transformation to apply.
+	 */
+	void transform(AffineMatrix2 T);
+
+}
