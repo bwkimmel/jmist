7ccd8bb4f51728ec78889489fe519458ac150a4a
Brad Kimmel <intrepidca@gmail.com>
2011-09-21 17:54:43 +0000

    Fixed ConductiveMaterial to use newer Material interface.

--- src/ca/eandb/jmist/framework/material/ConductiveMaterial.java
+++ src/ca/eandb/jmist/framework/material/ConductiveMaterial.java
@@ -4,15 +4,14 @@
 package ca.eandb.jmist.framework.material;
 
 import ca.eandb.jmist.framework.Medium;
-import ca.eandb.jmist.framework.Random;
 import ca.eandb.jmist.framework.ScatteredRay;
-import ca.eandb.jmist.framework.ScatteredRayRecorder;
 import ca.eandb.jmist.framework.SurfacePoint;
 import ca.eandb.jmist.framework.color.Color;
 import ca.eandb.jmist.framework.color.ColorModel;
 import ca.eandb.jmist.framework.color.ColorUtil;
 import ca.eandb.jmist.framework.color.Spectrum;
 import ca.eandb.jmist.framework.color.WavelengthPacket;
+import ca.eandb.jmist.framework.random.RandomUtil;
 import ca.eandb.jmist.math.Complex;
 import ca.eandb.jmist.math.Optics;
 import ca.eandb.jmist.math.Point3;
@@ -80,7 +79,7 @@ public final class ConductiveMaterial extends AbstractMaterial {
 	 * @see ca.eandb.jmist.framework.material.AbstractMaterial#scatter(ca.eandb.jmist.framework.SurfacePoint, ca.eandb.jmist.math.Vector3, ca.eandb.jmist.framework.color.WavelengthPacket, ca.eandb.jmist.framework.Random, ca.eandb.jmist.framework.ScatteredRayRecorder)
 	 */
 	@Override
-	public void scatter(SurfacePoint x, Vector3 v, WavelengthPacket lambda, Random rng, ScatteredRayRecorder recorder) {
+	public ScatteredRay scatter(SurfacePoint x, Vector3 v, boolean adjoint, WavelengthPacket lambda, double ru, double rv, double rj) {
 
 		ColorModel	cm			= lambda.getColorModel();
 		Point3		p			= x.getPosition();
@@ -93,43 +92,48 @@ public final class ConductiveMaterial extends AbstractMaterial {
 		boolean		fromSide	= x.getNormal().dot(v) < 0.0;
 		Color		R			= MaterialUtil.reflectance(v, n1, k1, n2, k2, normal);
 		Color		T			= cm.getWhite(lambda).minus(R);
+		double		r			= ColorUtil.getMeanChannelValue(R);
 
-		{
+		if (RandomUtil.bernoulli(r, rj)) {
 			Vector3		out		= Optics.reflect(v, normal);
 			boolean		toSide	= x.getNormal().dot(out) >= 0.0;
 
 			if (fromSide == toSide) {
-				recorder.add(ScatteredRay.specular(new Ray3(p, out), R, 1.0));
+				return ScatteredRay.specular(new Ray3(p, out), R.divide(r), r);
 			}
-		}
-
-		if (alpha != null) {
-			if (disperse) {
-				for (int i = 0, channels = cm.getNumChannels(); i < channels; i++) {
-					Complex		eta1	= new Complex(n1.getValue(i), k1.getValue(i));
-					Complex		eta2	= new Complex(n2.getValue(i), k2.getValue(i));
+		} else {
+	
+			if (alpha != null) {
+//				if (disperse) {
+//					for (int i = 0, channels = cm.getNumChannels(); i < channels; i++) {
+//						Complex		eta1	= new Complex(n1.getValue(i), k1.getValue(i));
+//						Complex		eta2	= new Complex(n2.getValue(i), k2.getValue(i));
+//						Vector3		out		= Optics.refract(v, eta1, eta2, normal);
+//						boolean		toSide	= x.getNormal().dot(out) >= 0.0;
+//	
+//						if (fromSide != toSide) {
+//							recorder.add(ScatteredRay.transmitSpecular(new Ray3(p, out), T.disperse(i), 1.0));
+//						}
+//					}
+//				} else { // !disperse
+					double		n1avg	= ColorUtil.getMeanChannelValue(n1);
+					double		k1avg	= ColorUtil.getMeanChannelValue(k1);
+					double		n2avg	= ColorUtil.getMeanChannelValue(n2);
+					double		k2avg	= ColorUtil.getMeanChannelValue(k2);
+					Complex		eta1	= new Complex(n1avg, k1avg);
+					Complex		eta2	= new Complex(n2avg, k2avg);
 					Vector3		out		= Optics.refract(v, eta1, eta2, normal);
 					boolean		toSide	= x.getNormal().dot(out) >= 0.0;
-
+	
 					if (fromSide != toSide) {
-						recorder.add(ScatteredRay.transmitSpecular(new Ray3(p, out), T.disperse(i), 1.0));
+						return ScatteredRay.transmitSpecular(new Ray3(p, out), T.divide(1 - r), 1 - r);
 					}
-				}
-			} else { // !disperse
-				double		n1avg	= ColorUtil.getMeanChannelValue(n1);
-				double		k1avg	= ColorUtil.getMeanChannelValue(k1);
-				double		n2avg	= ColorUtil.getMeanChannelValue(n2);
-				double		k2avg	= ColorUtil.getMeanChannelValue(k2);
-				Complex		eta1	= new Complex(n1avg, k1avg);
-				Complex		eta2	= new Complex(n2avg, k2avg);
-				Vector3		out		= Optics.refract(v, eta1, eta2, normal);
-				boolean		toSide	= x.getNormal().dot(out) >= 0.0;
-
-				if (fromSide != toSide) {
-					recorder.add(ScatteredRay.transmitSpecular(new Ray3(p, out), T, 1.0));
-				}
+//				}
 			}
+			
 		}
+		
+		return null;
 
 	}
 
