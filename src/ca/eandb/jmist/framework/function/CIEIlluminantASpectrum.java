94a21feb691da68ef4539b676c564658648d0289
Brad Kimmel <intrepidca@gmail.com>
2014-02-21 04:23:32 -0500

    Added CIE A and D-series illuminant SPDs

---
+++ src/ca/eandb/jmist/framework/function/CIEIlluminantASpectrum.java
@@ -0,0 +1,53 @@
+/**
+ * Java Modular Image Synthesis Toolkit (JMIST)
+ * Copyright (C) 2008-2014 Bradley W. Kimmel
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package ca.eandb.jmist.framework.function;
+
+import ca.eandb.jmist.framework.Function1;
+
+/**
+ * Spectral power distribution for CIE Illuminant A.
+ * @see http://en.wikipedia.org/wiki/Standard_illuminant#Illuminant_A
+ */
+public final class CIEIlluminantASpectrum implements Function1 {
+
+	/** Serialization version ID. */
+	private static final long serialVersionUID = 5075105015802796920L;
+
+	/** Numerator in formula for CIE Illuminant A distribution. */
+	private static final double NUMERATOR = Math.exp(1.435e7 / (2848.0 * 560.0)) - 1.0;
+
+	/* (non-Javadoc)
+	 * @see ca.eandb.jmist.framework.Function1#evaluate(double)
+	 */
+	@Override
+	public double evaluate(double x) {
+		double lambda = x * 1e9; // wavelength (in nm)
+		double denominator = Math.exp(1.435e7 / (2848.0 * lambda)) - 1.0;
+
+		return 100.0 * Math.pow(560.0 / lambda, 5.0) * (NUMERATOR / denominator);
+	}
+
+}
